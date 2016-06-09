b261f9c810a2ab20a3e9e8b56639cd1da65405d5
Adrien Grandemange <adrien.grandemange@oyez.fr>
2016-06-09 14:27:56 +0200

    More refactoring

--- src/island/ground.js
+++ src/island/ground.js
@@ -8,64 +8,68 @@ export function loadGround(layout, palette, ground_texture) {
         map: ground_texture
     });
     const geometry = new THREE.Geometry();
-    const {vertices, faces, faceVertexUvs} = geometry;
-    geometry.colorsNeedUpdate = true;
-    geometry.uvsNeedUpdate = true;
 
-    _.each(layout, (section, s_idx) => {
+    _.each(layout, (section) => {
         _.each(section.heightmap, (height, idx) => {
             const x = section.x * 64 + (65 - Math.floor(idx / 65));
             const y = section.y * 64 + (idx % 65);
-            vertices.push(new THREE.Vector3(x, height / 256.0 / 1.5, y));
+            geometry.vertices.push(new THREE.Vector3(x, height / 256.0 / 1.5, y));
         });
 
-        const s_offset = s_idx * 65 * 65;
         for (let x = 0; x < 64; ++x) {
             for (let y = 0; y < 64; ++y) {
-                const triangle = (idx) => new Triangle(section.triangles[(x * 64 + y) * 2 + idx]);
-                const t0 = triangle(0);
-                const t1 = triangle(1);
-                const r = t0.orientation;
-                const s = 1 - r;
-                const pt = (sx, sy) => s_offset + (x + sx) * 65 + y + sy;
-                //textureInfo[tri[t].textureIndex].uv[uvOrder[i]].u
-                if (t0.useColor || t0.useTexture) {
-                    faces.push(new THREE.Face3(pt(0, s), pt(r, 0), pt(s, 1), null, t0.color(palette)));
-                    faceVertexUvs[0].push([new THREE.Vector2(0, s), new THREE.Vector2(r, 0), new THREE.Vector2(s, 1)]);
-                }
-                if (t1.useColor || t1.useTexture) {
-                    faces.push(new THREE.Face3(pt(1, r), pt(s, 1), pt(r, 0), null, t1.color(palette)));
-                    faceVertexUvs[0].push([new THREE.Vector2(1, r), new THREE.Vector2(s, 1), new THREE.Vector2(r, 0)]);
-                }
+                const quad = loadQuad(section, palette, x, y);
+                geometry.faces.push.apply(geometry.faces, quad.faces);
+                geometry.faceVertexUvs[0].push.apply(geometry.faceVertexUvs[0], quad.uvs);
             }
         }
     });
 
+    geometry.colorsNeedUpdate = true;
+    geometry.uvsNeedUpdate = true;
     geometry.computeBoundingSphere();
     return new THREE.Mesh(geometry, material);
 }
 
-class Triangle {
-    constructor(t) {
-        // 0 -> 4:
-        this.textureBank = t & 0xF;
-        // 4 -> 6:
-        this.useTexture = (t & 0x30) >> 4;
-        // 6 -> 8:
-        this.useColor = (t & 0xC0) >> 6;
-        // 16 -> 17:
-        this.orientation = (t & 0x10000) >> 16;
-        // 19 -> 32:
-        this.textureIndex = (t & 0xFFF80000) >> 19;
+function loadQuad(section, palette, x, y) {
+    const quad = {
+        faces: [],
+        uvs: []
+    };
+    const t0 = loadTriangle(section, x, y, 0);
+    const t1 = loadTriangle(section, x, y, 1);
+    const r = t0.orientation;
+    const s = 1 - r;
+    const point = (xi, yi) => section.index * 65 * 65 + (x + xi) * 65 + y + yi;
+    //textureInfo[tri[t].textureIndex].uv[uvOrder[i]].u
+    if (t0.useColor || t0.useTexture) {
+        quad.faces.push(new THREE.Face3(point(0, s), point(r, 0), point(s, 1), null, getColor(t0, palette)));
+        quad.uvs.push([new THREE.Vector2(0, s), new THREE.Vector2(r, 0), new THREE.Vector2(s, 1)]);
     }
+    if (t1.useColor || t1.useTexture) {
+        quad.faces.push(new THREE.Face3(point(1, r), point(s, 1), point(r, 0), null, getColor(t1, palette)));
+        quad.uvs.push([new THREE.Vector2(1, r), new THREE.Vector2(s, 1), new THREE.Vector2(r, 0)]);
+    }
+    return quad;
+}
 
-    color(palette) {
-        if (this.useColor) {
-            const idx = this.textureBank * 16;
-            const r = palette[idx];
-            const g = palette[idx + 1];
-            const b = palette[idx + 2];
-            return new THREE.Color(r << 24 + g << 16 + b << 8);
-        }
+function loadTriangle(section, x, y, idx) {
+    const t = section.triangles[(x * 64 + y) * 2 + idx];
+    return {
+        textureBank: t & 0xF, // 0 -> 4
+        useTexture: (t & 0x30) >> 4, // 4 -> 6
+        useColor: (t & 0xC0) >> 6, // 6 -> 8
+        orientation: (t & 0x10000) >> 16, // 16 -> 17
+        textureIndex: (t & 0xFFF80000) >> 19 // 19 -> 32
+    };
+}
+
+function getColor(triangle, palette) {
+    if (triangle.useColor) {
+        const idx = triangle.textureBank * 16;
+        const r = palette[idx];
+        const g = palette[idx + 1];
+        const b = palette[idx + 2];
+        return new THREE.Color(r << 24 + g << 16 + b << 8);
     }
 }
--- src/island/layout.js
+++ src/island/layout.js
@@ -1,6 +1,7 @@
 export function loadLayout(ile) {
     const layout_raw = new Uint8Array(ile.getEntry(0));
     const layout = [];
+    let index = 0;
     for (let i = 0; i < 256; ++i) {
         const x = Math.floor(i / 16);
         const y = i % 16;
@@ -8,13 +9,14 @@ export function loadLayout(ile) {
             const id = layout_raw[i];
             layout.push({
                 id: id,
+                index: index++,
                 x: (16 - x) - 8,
                 y: y - 8,
                 triangles: new Uint32Array(ile.getEntry(id * 6 - 1)),
                 textureInfo: new Uint16Array(ile.getEntry(id * 6)),
                 heightmap: new Uint16Array(ile.getEntry(id * 6 + 1)),
                 intensity: new Uint8Array(ile.getEntry(id * 6 + 2))
-            })
+            });
         }
     }
     return layout;
