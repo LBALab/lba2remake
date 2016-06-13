343c1b3a7f6f38bd122e07404e9d2b921b94a4c7
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-06-13 23:10:50 +0200

    Toggle palette

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import _ from 'lodash';
 import {loadHqrAsync} from './hqr';
 
 class ThreeContainer extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = this.getComponentSize();
@@ -42,11 +41,37 @@ class ThreeContainer extends React.Component {
 }
 
 class Main extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            showPalette: false
+        };
+    }
+
+    computeChartData(offset) {
+        const data = {
+            red: [],
+            green: [],
+            blue: []
+        };
+        for (let i = 0; i < 16; ++i) {
+            data.red.push(this.src[(i + offset) * 3]);
+            data.green.push(this.src[(i + offset) * 3 + 1]);
+            data.blue.push(this.src[(i + offset) * 3 + 2]);
+        }
+        return data;
+    }
 
     componentDidMount() {
         const node = this.refs.pal;
         const ctx = node.getContext('2d');
         const chartNode = this.refs.chart;
+        const that = this;
+        window.addEventListener('keydown', function(event) {
+            if (event.keyCode == 80){
+                that.setState({showPalette: !(that.state.showPalette)});
+            }
+        });
         loadHqrAsync('RESS.hqr')(function(err, hqr) {
             const src = new Uint8Array(hqr.getEntry(0));
             const tgt = new Uint8ClampedArray(256 * 4);
@@ -59,19 +84,11 @@ class Main extends React.Component {
             const idt = new ImageData(tgt, 16, 16);
             createImageBitmap(idt).then(function(response) {
                 ctx.imageSmoothingEnabled = false;
-                ctx.drawImage(response, 0, 0, 192, 192);
+                ctx.drawImage(response, 0, 0, 256, 256);
             });
-
-            const red = [];
-            const green = [];
-            const blue = [];
-            const offset = 16;
-            for (let i = 0; i < 16; ++i) {
-                red.push(src[(i + offset) * 3]);
-                green.push(src[(i + offset) * 3 + 1]);
-                blue.push(src[(i + offset) * 3 + 2]);
-            }
-            new Chart(chartNode, {
+            that.src = src;
+            const {red, green, blue} = that.computeChartData(0);
+            that.chart = new Chart(chartNode, {
                 type: 'line',
                 data: {
                     labels: _.range(16),
@@ -100,24 +117,39 @@ class Main extends React.Component {
         });
     }
 
+    onClick(event) {
+        const x = event.clientX >> 4;
+        const y = event.clientY >> 4;
+        const {red, green, blue} = this.computeChartData(y * 16);
+        this.chart.data.datasets[0].data = red;
+        this.chart.data.datasets[1].data = green;
+        this.chart.data.datasets[2].data = blue;
+        this.chart.update();
+    }
+
     render() {
-        const canvas_style = {
+        const pal_visibility = this.state.showPalette ? 'block' : 'none';
+        const pal_style = {
             position: 'absolute',
             top: 0,
             left: 0,
-            width: 192,
-            height: 192
+            width: 256,
+            height: 256,
+            display: pal_visibility
         };
-        const canvas_style2 = {
+        const chart_style = {
             position: 'absolute',
             top: 0,
             right: 0,
-            width: 256,
-            height: 192
+            width: 512,
+            height: 512,
+            background: 'rgba(255, 255, 255, 0.4)',
+            display: pal_visibility
         };
         return <div>
-            <canvas ref='pal' width="192" height="192" style={canvas_style}/>
-            <canvas ref='chart' width="256" height="192" style={canvas_style2}/>
+            <ThreeContainer/>
+            <canvas ref='pal' width="256" height="256" onClick={this.onClick.bind(this)} style={pal_style}/>
+            <canvas ref='chart' width="512" height="512" style={chart_style}/>
         </div>;
     }
 }
