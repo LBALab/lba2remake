36d9c5011dd665f5f848a74d3a23828c9114b549
Adrien Grandemange <adrien.grandemange@oyez.fr>
2016-06-13 18:03:39 +0200

    Plot palette

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Renderer from './renderer';
+import Chart from 'chart.js';
+import _ from 'lodash';
 import {loadHqrAsync} from './hqr';
 
 class ThreeContainer extends React.Component {
@@ -43,9 +45,10 @@ class Main extends React.Component {
 
     componentDidMount() {
         const node = this.refs.pal;
-        const ctx  =node.getContext('2d');
-        loadHqrAsync('RESS.hqr')(function(err, data) {
-            const src = new Uint8Array(data.getEntry(0));
+        const ctx = node.getContext('2d');
+        const chartNode = this.refs.chart;
+        loadHqrAsync('RESS.hqr')(function(err, hqr) {
+            const src = new Uint8Array(hqr.getEntry(0));
             const tgt = new Uint8ClampedArray(256 * 4);
             for (let i = 0; i < 256; ++i) {
                 tgt[i * 4] = src[i * 3];
@@ -56,7 +59,43 @@ class Main extends React.Component {
             const idt = new ImageData(tgt, 16, 16);
             createImageBitmap(idt).then(function(response) {
                 ctx.imageSmoothingEnabled = false;
-                ctx.drawImage(response, 0, 0, 128, 128);
+                ctx.drawImage(response, 0, 0, 192, 192);
+            });
+
+            const red = [];
+            const green = [];
+            const blue = [];
+            const offset = 16;
+            for (let i = 0; i < 16; ++i) {
+                red.push(src[(i + offset) * 3]);
+                green.push(src[(i + offset) * 3 + 1]);
+                blue.push(src[(i + offset) * 3 + 2]);
+            }
+            new Chart(chartNode, {
+                type: 'line',
+                data: {
+                    labels: _.range(16),
+                    datasets: [
+                        {
+                            borderColor: 'red',
+                            label: 'R',
+                            data: red
+                        },
+                        {
+                            borderColor: 'green',
+                            label: 'G',
+                            data: green
+                        },
+                        {
+                            borderColor: 'blue',
+                            label: 'B',
+                            data: blue
+                        }
+                    ]
+                },
+                options: {
+                    responsive: false
+                }
             });
         });
     }
@@ -66,12 +105,19 @@ class Main extends React.Component {
             position: 'absolute',
             top: 0,
             left: 0,
-            width: 128,
-            height: 128
+            width: 192,
+            height: 192
+        };
+        const canvas_style2 = {
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            width: 256,
+            height: 192
         };
         return <div>
-            <ThreeContainer/>
-            <canvas ref='pal' width="128" height="128" style={canvas_style}/>
+            <canvas ref='pal' width="192" height="192" style={canvas_style}/>
+            <canvas ref='chart' width="256" height="192" style={canvas_style2}/>
         </div>;
     }
 }
