e2bb5e410edb7493cb1e24dadfcaf60459cc3741
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-06-17 00:58:08 +0200

    Parse island objects metadata

--- src/island/objects.js
+++ src/island/objects.js
@@ -2,5 +2,19 @@ const push = Array.prototype.push;
 
 export function loadObjects(island, section, geometry) {
     const numObjects = section.info.numObjects;
-    console.log('numObjects', numObjects);
+    for (let i = 0; i < numObjects; ++i) {
+        const info = loadObjectInfo(section.objects, i);
+        console.log(info);
+    }
+}
+
+function loadObjectInfo(objects, index) {
+    const offset = index * 48;
+    return {
+        index: objects.getUint32(offset, true),
+        x: objects.getInt32(offset + 4, true),
+        y: objects.getInt32(offset + 8, true),
+        z: objects.getInt32(offset + 12, true),
+        angle: objects.getUint8(offset + 17)
+    }
 }
