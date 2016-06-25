11b2efcd09041e3bc9babf3d127bd70092262ffa
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-06-25 18:41:08 +0200

    Refactor shader

--- src/island/shaders/ground.frag.glsl
+++ src/island/shaders/ground.frag.glsl
@@ -5,14 +5,23 @@ uniform sampler2D tiles;
 varying vec4 vColor;
 varying vec2 vUv;
 
+const float epsilon = 0.05;
+
+bool equals(float value, float refValue) {
+    return (abs(value - refValue) <= epsilon);
+}
+
+const float COLOR = 0.0;
+const float TEXTURE = 1.0;
+
 void main() {
     vec3 color = vColor.rgb;
-    float flag = vColor.a;
-    if (flag < 0.1) {
+    float type = vColor.a;
+    if (equals(type, COLOR)) {
         gl_FragColor = vec4(color, 1.0);
     } else {
         vec4 tex = texture2D(tiles, vUv);
-        if (flag > 0.9) {
+        if (equals(type, TEXTURE)) {
             gl_FragColor = vec4(tex.rgb * color.r, 1.0);
         } else {
             gl_FragColor = vec4(mix(color, tex.rgb * length(color), tex.a), 1.0);
