36614d1693d8a15dcbe3fe2ced02b8d077609697
xesf <xesfnet@gmail.com>
2016-07-06 23:20:24 +0100

    More body load entries. Having an issue with the Sphere sections.

--- src/model/body2.js
+++ src/model/body2.js
@@ -32,11 +32,11 @@ export function loadBody2(model, objects, index) {
          
         loadBones(obj);
         loadVertices(obj);
-        //loadNormals(obj);
-        //loadPolygons(obj);
-        //loadLines(obj);
-        //loadSpheres(obj);
-        //loadTextures(obj);
+        loadNormals(obj);
+        loadPolygons(obj);
+        loadLines(obj);
+        loadSpheres(obj);
+        loadTextures(obj);
         
         objects[index] = obj;
         return obj;
@@ -45,9 +45,9 @@ export function loadBody2(model, objects, index) {
 
 function loadBones(object) {
     object.bones = [];
-    const rawBones = new Uint16Array(object.buffer, object.bonesOffset, object.bonesSize * 4);
+    const rawBones = new Uint16Array(object.buffer, object.bonesOffset, object.bonesSize * 8);
     for (let i = 0; i < object.bonesSize; ++i) {
-        const index = i * 4;
+        const index = i * 8;
         object.bones.push({
             parent: rawBones[index],
             vertex: rawBones[index + 1],
@@ -59,9 +59,9 @@ function loadBones(object) {
 
 function loadVertices(object) {
     object.vertices = [];
-    const rawVertices = new Uint16Array(object.buffer, object.verticesOffset, object.verticesSize * 4);
+    const rawVertices = new Uint16Array(object.buffer, object.verticesOffset, object.verticesSize * 8);
     for (let i = 0; i < object.verticesSize; ++i) {
-        const index = i * 4;
+        const index = i * 8;
         object.vertices.push({
             x: rawVertices[index],
             y: rawVertices[index + 1],
@@ -71,7 +71,82 @@ function loadVertices(object) {
     }
 }
 
+function loadNormals(object) {
+    object.normals = [];
+    const rawNormals = new Uint16Array(object.buffer, object.normalsOffset, object.normalsSize * 8);
+    for (let i = 0; i < object.normalsSize; ++i) {
+        const index = i * 8;
+        object.normals.push({
+            x: rawNormals[index],
+            y: rawNormals[index + 1],
+            z: rawNormals[index + 2],
+            colour: rawNormals[index + 3]
+        });
+    }
+}
 
+function loadPolygons(object) {
+    object.polygons = [];
+    // const rawPolygons = new Uint16Array(object.buffer, object.polygonsOffset, object.polygonsSize * 4);
+    // for (let i = 0; i < object.polygonsSize; ++i) {
+    //     const index = i * 4;
+    //     let flag = rawPolygons[index]; // flag to indicate if has texture or transparency
+    //     let size = rawPolygons[index + 1];
+    //     let num = rawPolygons[index + 2]; // total number of polygons
+    //     let unk1 = rawPolygons[index + 3];
+
+    //     for (let i = 0; i < object.polygonsSize; ++i) {
+        
+    //     object.polygons.push({
+    //         x: rawPolygons[index],
+    //         y: rawPolygons[index + 1],
+    //         z: rawPolygons[index + 2],
+    //         colour: rawPolygons[index + 3]
+    //     });
+    // }
+}
+
+function loadLines(object) {
+    object.lines = [];
+    const rawLines = new Uint16Array(object.buffer, object.linesOffset, object.linesSize * 8);
+    for (let i = 0; i < object.linesSize; ++i) {
+        const index = i * 8;
+        object.lines.push({
+            unk1: rawLines[index],
+            colour: rawLines[index + 1],
+            vertex1: rawLines[index + 2],
+            vertex2: rawLines[index + 3]
+        });
+    }
+}
+
+function loadSpheres(object) {
+    object.spheres = [];
+    const rawSpheres = new Uint16Array(object.buffer, object.spheresOffset, object.spheresSize * 8);
+    for (let i = 0; i < object.spheresSize; ++i) {
+        const index = i * 8;
+        object.spheres.push({
+            unk1: rawSpheres[index],
+            colour: rawSpheres[index + 1],
+            vertex: rawSpheres[index + 2],
+            size: rawSpheres[index + 3]
+        });
+    }
+}
+
+function loadTextures(object) {
+    object.textures = [];
+    const rawTextures = new Uint8Array(object.buffer, object.texturesOffset, object.texturesSize * 4);
+    for (let i = 0; i < object.texturesSize; ++i) {
+        const index = i * 4;
+        object.textures.push({
+            x: rawTextures[index],
+            y: rawTextures[index + 1],
+            w: rawTextures[index + 2],
+            h: rawTextures[index + 3]
+        });
+    }
+}
 
 function getPosition(object, info, index) {
     const pos = rotate([
