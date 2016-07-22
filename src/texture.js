7e64e5f2801d461c38b4ffce8d3ea098346c8e07
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-07-22 21:48:02 +0200

    Remove subtexture function. Mitigate texture bleeding by not using mipmaps.

--- src/texture.js
+++ src/texture.js
@@ -27,39 +27,9 @@ export function loadTexture(buffer, palette) {
         THREE.RepeatWrapping,
         THREE.RepeatWrapping,
         THREE.NearestFilter,
-        THREE.LinearMipMapLinearFilter
+        THREE.LinearFilter
     );
     texture.needsUpdate = true;
-    texture.generateMipmaps = true;
-    return texture;
-}
-
-export function loadSubTexture(buffer, palette, x_offset, y_offset, width, height) {
-    const pixel_data = new Uint8Array(buffer);
-    const image_data = new Uint8Array(width * height * 4);
-    for (let y = 0; y < height; ++y) {
-        for (let x = 0; x < width; ++x) {
-            const src_i = (y + y_offset) * 256 + x + x_offset;
-            const tgt_i = y * width + x;
-            image_data[tgt_i * 4] = palette[pixel_data[src_i] * 3];
-            image_data[tgt_i * 4 + 1] = palette[pixel_data[src_i] * 3 + 1];
-            image_data[tgt_i * 4 + 2] = palette[pixel_data[src_i] * 3 + 2];
-            image_data[tgt_i * 4 + 3] = 0xFF;
-        }
-    }
-    const texture = new THREE.DataTexture(
-        image_data,
-        width,
-        height,
-        THREE.RGBAFormat,
-        THREE.UnsignedByteType,
-        THREE.UVMapping,
-        THREE.RepeatWrapping,
-        THREE.RepeatWrapping,
-        THREE.LinearFilter,
-        THREE.LinearMipMapLinearFilter
-    );
-    texture.needsUpdate = true;
-    texture.generateMipmaps = true;
+    texture.generateMipmaps = false;
     return texture;
 }
