23fb0e5c4744ee3509cf6be3afa807bf56b439b0
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-08-04 00:26:23 +0200

    Half pixel trick on ground tiles too

--- src/island/shaders/textured.frag.glsl
+++ src/island/shaders/textured.frag.glsl
@@ -8,7 +8,8 @@ varying vec2 vUv;
 #require "./fog.frag"
 
 void main() {
-    vec4 tex = texture2D(texture, vUv);
+    vec2 uv = vUv * 0.99609375 + vec2(0.001953125);
+    vec4 tex = texture2D(texture, uv);
     vec3 color = mix(vColor.rgb, tex.rgb * vColor.a, tex.a);
     gl_FragColor = vec4(fog(color), 1.0);
 }
