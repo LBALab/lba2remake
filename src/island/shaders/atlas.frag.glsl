bbb75a99525cc397bdd84305f2eddf5ff0be4b4f
Adrien Grandemange <adrien.grandemange@oyez.fr>
2016-08-03 14:03:05 +0200

    Simplify shader code

--- src/island/shaders/atlas.frag.glsl
+++ src/island/shaders/atlas.frag.glsl
@@ -7,7 +7,7 @@ varying vec2 vUv;
 varying vec4 vUvGroup;
 
 void main() {
-    vec2 uv = vec2(mod(vUv[0], vUvGroup[2]) + vUvGroup[0], mod(vUv[1], vUvGroup[3]) + vUvGroup[1]);
+    vec2 uv = mod(vUv, vUvGroup.zw) + vUvGroup.xy;
     vec4 tex = texture2D(texture, uv);
     gl_FragColor = vec4(mix(vColor.rgb, tex.rgb * vColor.a, tex.a), tex.a);
 }
