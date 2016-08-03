4885d54fecc567cd2d7c2b373f7ba0cc0b737a56
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-08-04 00:12:17 +0200

    Half pixel offset in texture UVs to avoid bleeding

--- src/island/shaders/atlas.frag.glsl
+++ src/island/shaders/atlas.frag.glsl
@@ -9,7 +9,7 @@ varying vec4 vUvGroup;
 #require "./fog.frag"
 
 void main() {
-    vec2 uv = mod(vUv, vUvGroup.zw) + vUvGroup.xy;
+    vec2 uv = mod(vUv, vUvGroup.zw) * 0.99609375 + vUvGroup.xy + vec2(0.001953125);
     vec4 tex = texture2D(texture, uv);
     vec3 color = mix(vColor.rgb, tex.rgb * vColor.a, tex.a);
     gl_FragColor = vec4(fog(color), tex.a);
