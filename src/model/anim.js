dd28441712ac0f384b90bcb7d1364f27b3ae19a6
xesf <xesfnet@gmail.com>
2016-08-06 22:27:20 +0100

    Load animation data

--- src/model/anim.js
+++ src/model/anim.js
@@ -12,7 +12,7 @@ export function loadAnim(model, body, anims, index) {
         const data = new DataView(buffer);
         const obj = {
             numKeyframes: data.getUint16(0x00, true),
-            numBones: data.getUint16(0x02, true),
+            numBoneframes: data.getUint16(0x02, true),
             startFrame: data.getUint16(0x04, true),
             unk1: data.getUint16(0x08, true),
             
@@ -28,10 +28,62 @@ export function loadAnim(model, body, anims, index) {
 
 /** Load anim key frames */
 function loadKeyframes(object) {
-    
+    object.keyframes = [];
+    const data = new DataView(object.buffer, 0, object.buffer.length);
+    let offset = 0;
+    for (let i = 0; i < object.numKeyframes; ++i) {
+        let keyframe = {
+            length: data.getUint16(offset, true),
+            x: data.getUint16(offset + 2, true),
+            y: data.getUint16(offset + 4, true),
+            z: data.getUint16(offset + 6, true),
+            canFall: false,
+            boneframes: []
+        };
+        offset += 8;
+
+        for (let j = 0; j < object.numBoneframes; ++j) {
+            const {boneframe, canFall} = loadBoneframe(keyframe, data, offset);
+            keyframe.canFall |= canFall;
+            offset += 8;
+            keyframe.boneframes.push(boneframe);
+        }
+
+        object.keyframes.push(keyframe);
+    }
 }
 
-/** Load bones at keyframe */
-function loadBoneframes(object) {
-    
+/** Load bone at keyframe */
+function loadBoneframe(keyframe, data, offset) {
+    let boneframe = {
+        type: data.getUint16(offset, true), // if > 0 canFall because it has translation in space
+        x: 0,
+        y: 0,
+        z: 0,
+        angleX: 0,
+        angleY: 0,
+        angleZ: 0
+    };
+    let canFall = false;
+
+    const x = data.getUint16(offset + 2, true);
+    const y = data.getUint16(offset + 4, true);
+    const z = data.getUint16(offset + 6, true);
+
+    // assigned based on type of bone animation (rotation or translation)
+    switch (boneframe.type) {
+        case 0: // rotation
+            boneframe.angleX = x * (360 / 0x1000);
+            boneframe.angleY = y * (360 / 0x1000);
+            boneframe.angleZ = z * (360 / 0x1000); 
+            break;
+        case 1:
+        case 2: // translation
+            boneframe.x = x;
+            boneframe.y = y;
+            boneframe.z = z;
+            canFall = true;
+            break;
+    }
+    return { boneframe, canFall };
 }
