fbe10c4b93b11d391d2daadbf8d747fba8d9f3de
Adrien Grandemange <adrien.grandemange@oyez.fr>
2016-08-08 14:33:54 +0200

    Shader fallback when extension not available

--- src/island/shaders/atlas.frag.glsl
+++ src/island/shaders/atlas.frag.glsl
@@ -1,5 +1,7 @@
-#extension GL_EXT_shader_texture_lod : enable
+#ifdef GL_EXT_shader_texture_lod
 #extension GL_OES_standard_derivatives : enable
+#extension GL_EXT_shader_texture_lod : enable
+#endif
 precision highp float;
 
 uniform sampler2D texture;
@@ -12,7 +14,11 @@ varying vec4 vUvGroup;
 
 void main() {
     vec2 uv = mod(vUv, vUvGroup.zw) + vUvGroup.xy;
+#ifdef GL_EXT_shader_texture_lod
     vec4 tex = texture2DGradEXT(texture, uv, dFdx(vUv), dFdy(vUv));
+#else
+    vec4 tex = texture2D(texture, uv);
+#endif
     vec3 color = mix(vColor.rgb, tex.rgb * vColor.a, tex.a);
     gl_FragColor = vec4(fog(color), tex.a);
 }
