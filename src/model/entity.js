dcc347526dc71b6d41193e742b106c0c67854c55
Alexandre Fontoura <xesfnet@gmail.com>
2016-08-13 19:01:39 +0100

    Properly implementing entity information. Not finished yet.

--- src/model/entity.js
+++ src/model/entity.js
@@ -2,22 +2,51 @@ import _ from 'lodash';
 
 const push = Array.prototype.push;
 
+const ACTIONTYPE = {
+    ACTION_HITTING           : 0,
+	ACTION_SAMPLE            : 1,
+    ACTION_SAMPLE_FREQ       : 2,
+    ACTION_THROW_EXTRA_BONUS : 3,
+	ACTION_THROW_MAGIC_BALL  : 4,
+	ACTION_SAMPLE_REPEAT     : 5,
+	ACTION_UNKNOWN_6         : 6,
+	ACTION_UNKNOWN_7         : 7,
+	ACTION_SAMPLE_STOP       : 8,
+	ACTION_UNKNOWN_9         : 9, // unused
+	ACTION_SAMPLE_BRICK_1    : 10,
+	ACTION_SAMPLE_BRICK_2    : 11,
+	ACTION_HERO_HITTING      : 12,
+	ACTION_UNKNOWN_13        : 13,
+	ACTION_UNKNOWN_14        : 14,
+	ACTION_UNKNOWN_15        : 15,
+}
+
 export function loadEntity(buffer) {
     let entities = [];
     const data = new DataView(buffer);
-    let offset = data.getUint32(0, true);
+    const offset = data.getUint32(0, true);
     const numEntries = (offset / 4) - 1;
+    let offsets = [];
     for (let i = 0; i < numEntries; ++i) {
-        const {entity, innerOffset} = loadEntityEntry(buffer, offset, i);
+        const entry = {
+            offset : data.getUint32(i * 4, true),
+            size: data.getUint32((i + 1) * 4, true) - data.getUint32(i * 4, true)
+        }
+        if (i == numEntries - 1) {
+            entry.size = buffer.length - entry.offset;
+        }
+        offsets.push(entry);
+    }
+    for (let i = 0; i < numEntries; ++i) {
+        const entity = loadEntityEntry(buffer, offsets[i].offset, i);
         entities.push(entity);
-        offset += innerOffset;
     }
     return entities;
 }
 
 function loadEntityEntry(buffer, offset, index) {
-    let innerOffset = 0;
     const data = new DataView(buffer, offset);
+    offset = 0;
     let entity = {
         index: index,
         bodies: [],
@@ -25,48 +54,42 @@ function loadEntityEntry(buffer, offset, index) {
     }
     let opcode = 0;
     do {
-        opcode = data.getUint8(innerOffset);
-        innerOffset++;
+        opcode = data.getUint8(offset++);
 
         switch(opcode) {
             case 1: { // body
-                let body = loadEntityBody(data, innerOffset);
+                let body = loadEntityBody(data, offset);
                 entity.bodies.push(body);
-                
-                innerOffset += 5;
-                if (body.hasCollisionBox) {
-                    innerOffset += 13;
-                }
+                offset += body.offset;
             }
             break;
             case 3: { // anim
-                let anim = loadEntityAnim(data, innerOffset);
+                let anim = loadEntityAnim(data, offset);
                 entity.anims.push(anim);
-                innerOffset += 5 + anim.size - 3;
+                offset += anim.offset;
             }
             break;
         }
     } while(opcode != 0xFF);
 
-    return {entity, innerOffset};
+    return entity;
 }
 
-function loadEntityBody(data, innerOffset) {
+function loadEntityBody(data, offset) {
     let body = {
         index: 0,
+        offset: 0,
         bodyIndex: 0,
         hasCollisionBox: false,
         box: {}
     }
 
-    body.index = data.getUint8(innerOffset);
-    innerOffset++;
-    innerOffset++; // skip this unknown byte
-    body.bodyIndex = data.getUint16(innerOffset);
-    innerOffset += 2;
+    body.index = data.getUint8(offset++);
+    body.offset = data.getUint8(offset++) + 1; // to add the previous byte
+    body.bodyIndex = data.getUint16(offset);
+    offset += 2;
 
-    const hasCollisionBox = data.getUint8(innerOffset);
-    innerOffset++;
+    const hasCollisionBox = data.getUint8(offset++);
 
     if (hasCollisionBox == 1) {
         body.hasCollisionBox = true;
@@ -75,44 +98,75 @@ function loadEntityBody(data, innerOffset) {
             tX: 0, tY: 0, tZ: 0
         }
         
-        innerOffset++; // skip number bytes to read
+        const innerOffset = data.getUint8(offset++);
 
-        box.bX = data.getUint16(innerOffset);
-        innerOffset += 2;
-        box.bY = data.getUint16(innerOffset);
-        innerOffset += 2;
-        box.bZ = data.getUint16(innerOffset);
-        innerOffset += 2;
-        box.tX = data.getUint16(innerOffset);
-        innerOffset += 2;
-        box.tY = data.getUint16(innerOffset);
-        innerOffset += 2;
-        box.tZ = data.getUint16(innerOffset);
-        innerOffset += 2;
+        box.bX = data.getUint16(offset);
+        box.bY = data.getUint16(offset + 2);
+        box.bZ = data.getUint16(offset + 4);
+        box.tX = data.getUint16(offset + 6);
+        box.tY = data.getUint16(offset + 8);
+        box.tZ = data.getUint16(offset + 10);
 
         body.box = box;
+        offset += innerOffset;
     }
     return body;
 }
 
-function loadEntityAnim(data, innerOffset) {
+function loadEntityAnim(data, offset) {
     let anim = {
         index: 0,
+        offset: 0,
         animIndex: 0,
-        animSize: 0,
-        animType: 0
+        numActions: 0,
+        actions: []
     }
 
-    anim.index = data.getUint8(innerOffset);
-    innerOffset++;
-    innerOffset++; // skip this unknown byte (only on LBA2 - LBA1 doesn't have it)
-    anim.size = data.getUint8(innerOffset);
-    innerOffset++;
-    anim.animIndex = data.getUint16(innerOffset);
-    innerOffset += 2;
+    anim.index = data.getUint8(offset++);
+    anim.offset = data.getUint8(offset++) + 1; // to add the previous byte
+    anim.animIndex = data.getUint16(offset);
+    offset += 2;
+    anim.numActions = data.getUint8(offset++);
 
-    // TODO anim data
-    innerOffset += anim.size - 3;
+    /*for (let i = 0; i < numActions; ++i) {
+        let action = {
+            type: data.getUint8(offset++);
+        }
+        switch(type - 5) {
+            case ACTION_HITTING: 
+            break;
+            case ACTION_SAMPLE: 
+            break;
+            case ACTION_SAMPLE_FREQ: 
+            break;
+            case ACTION_THROW_EXTRA_BONUS: 
+            break;
+            case ACTION_THROW_MAGIC_BALL: 
+            break;
+            case ACTION_SAMPLE_REPEAT: 
+            break;
+            case ACTION_UNKNOWN_6: 
+            break;
+            case ACTION_UNKNOWN_7: 
+            break;
+            case ACTION_SAMPLE_STOP: 
+            break;
+            case ACTION_UNKNOWN_9: 
+            break;
+            case ACTION_SAMPLE_BRICK_1: 
+            break;
+            case ACTION_SAMPLE_BRICK_2: 
+            break;
+            case ACTION_HERO_HITTING: 
+            break;
+            case ACTION_UNKNOWN_13: 
+            break;
+            case ACTION_UNKNOWN_14: 
+            break;
+            case ACTION_UNKNOWN_15: 
+            break;
+        }
+    }*/
 
     return anim;
 }
