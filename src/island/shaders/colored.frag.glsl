e61c46f90a6890dbfd1706b75ba923842fa2d6e7
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-08-13 23:50:27 +0200

    Simple noise on plain colors

--- src/island/shaders/colored.frag.glsl
+++ src/island/shaders/colored.frag.glsl
@@ -1,9 +1,16 @@
+#ifdef GL_OES_standard_derivatives
+#extension GL_OES_standard_derivatives : enable
+#endif
 precision highp float;
 
 varying vec4 vColor;
+varying vec3 vPos;
 
 #require "./fog.frag"
+#require "./noise3D"
 
 void main() {
-    gl_FragColor = vec4(fog(vColor.rgb), 1.0);
+    float bias = max(1.0 - (max(length(dFdx(vPos)), length(dFdy(vPos))) * 512.0), 0.0);
+    vec3 color = vColor.rgb * ((snoise(vPos * 2048.0) * bias) * 0.2 + 0.9);
+    gl_FragColor = vec4(fog(color), 1.0);
 }
--- src/island/shaders/colored.vert.glsl
+++ src/island/shaders/colored.vert.glsl
@@ -7,8 +7,10 @@ attribute vec3 position;
 attribute vec4 color;
 
 varying vec4 vColor;
+varying vec3 vPos;
 
 void main() {
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     vColor = color;
+    vPos = position;
 }
