da8bff58cef17a8b517b05cf8a42ec79f44bf88c
Adrien Grandemange <adrien.grandemange@oyez.fr>
2016-08-18 14:42:12 +0200

    Much more efficient noise implementation (work in progress, looks quite ugly at this point)

--- src/island/shaders/dither.glsl
+++ src/island/shaders/dither.glsl
@@ -1,12 +1,16 @@
-#require "./noise3D"
+highp vec2 rand(vec2 co) {
+    highp vec2 a = vec2(12.9898, 78.233);
+    highp float b = 43758.5453;
+    highp float sn1 = mod(dot(floor(co), a), 3.14);
+    highp float sn2 = mod(dot(floor(co * 2.0), a), 3.14);
+    return fract(sin(vec2(sn1, sn2)) * b) * 2.0 - 1.0;
+}
 
 vec4 dither(vec2 uv) {
     float bias = clamp(max(length(dFdx(vPos)), length(dFdy(vPos))) * 900.0, 0.0, 1.0);
-    float level1 = pow(2.0, 12.0 - floor(bias * 5.0));
-    float level2 = level1 * 2.0;
-    float noise1 = (snoise(vPos * level1) - 0.5);
-    float noise2 = (snoise(vPos * level2) - 0.5);
-    float noise = mix(noise1, noise2, 1.0 - fract(bias * 5.0));
+    float level = pow(2.0, 12.0 - floor(bias * 5.0));
+    vec2 vnoise = rand(vPos.xz * level);
+    float noise = mix(vnoise[0], vnoise[1], 1.0 - fract(bias * 5.0));
     uv.x = uv.x + noise * 0.0625 * (2.0 - bias);
     uv += 0.03125;
     return texture2D(palette, uv);
