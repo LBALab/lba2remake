6c45274afc84af1d8ba5f2019abba7dfbc784fd7
Adrien Grandemange <adrien.grandemange@oyez.fr>
2016-08-18 13:56:43 +0200

    Pad texture color lookup too

--- src/island/shaders/texture2DPal.glsl
+++ src/island/shaders/texture2DPal.glsl
@@ -1,4 +1,4 @@
 vec4 texture2DPal(vec4 texInfo, vec2 uv) {
     float colorIndex = texInfo[0];
-    return texture2D(palette, vec2(mod(colorIndex * 256.0, 16.0) / 16.0 + vColorInfo.x / 32.0 - 0.5, floor(colorIndex * 16.0) / 16.0));
+    return texture2D(palette, vec2(mod(colorIndex * 256.0, 16.0) / 16.0 + vColorInfo.x / 32.0 - 0.5, floor(colorIndex * 16.0) / 16.0) + 0.03125);
 }
