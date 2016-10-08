4cfbf8886415b1804ebcc263da7aa19357fa034d
Alexandre Fontoura <xesfnet@gmail.com>
2016-10-09 00:58:51 +0100

    Showing points in the scenes.

--- src/game/points.js
+++ src/game/points.js
@@ -2,6 +2,12 @@ import async from 'async';
 import THREE from 'three';
 import _ from 'lodash';
 
+const geometry = new THREE.CylinderGeometry( 0, 0.01, 0.05, 8, 1, false );
+//const geometry = new THREE.BoxBufferGeometry(0.01,0.01,0.01);
+const material = new THREE.MeshBasicMaterial( {color: 0x0000ff } );
+material.depthTest = true;
+material.depthWrite = true;
+
 export function createPoint(currentScene, index, pointProps, xOffset, zOffset) {
     let point = pointProps;
     point.index = index;
@@ -15,12 +21,9 @@ export function createPoint(currentScene, index, pointProps, xOffset, zOffset) {
     point.physics.position.y = point.pos[1];
     point.physics.position.z = point.pos[2] + zOffset * 2;
 
-    const geometry = new THREE.ConeGeometry( 5, 20, 32 );
-    const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    const cone = new THREE.Mesh(geometry, material);
+    /*const obj = new THREE.Object3D();
+    obj.add(new THREE.Mesh(geometry, material));
+    obj.position.set(point.physics.position.x, point.physics.position.y, point.physics.position.z);
 
-    point.threeObject = cone;
-    point.threeObject.position.set(point.physics.position.x, point.physics.position.y, point.physics.position.z);
-
-    point.currentScene.threeScene.add(point.threeObject);
+    point.currentScene.threeScene.add(obj);*/
 }
