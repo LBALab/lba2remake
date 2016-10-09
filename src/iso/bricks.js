7cfeccb6eca28a1c8dad5a4b3af990f30ceb2316
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-10-09 22:53:40 +0200

    More refactoring

---
+++ src/iso/bricks.js
@@ -0,0 +1,46 @@
+import {bits} from '../utils';
+
+export function loadBricks(bkg) {
+    const bricks = [];
+    for (let i = 197; i <= 18099; ++i) {
+        bricks.push(loadBrick(bkg, i));
+    }
+    return bricks;
+}
+
+function loadBrick(bkg, entry) {
+    const dataView = new DataView(bkg.getEntry(entry));
+    const height = dataView.getUint8(1);
+    const offsetX = dataView.getUint8(2);
+    const offsetY = dataView.getUint8(3);
+    const buffer = new ArrayBuffer(48 * 38);
+    const pixels = new Uint8Array(buffer);
+    let ptr = 4;
+    for (let y = 0; y < height; ++y) {
+        const numRuns = dataView.getUint8(ptr++);
+        let x = 0;
+        const offset = () => (y + offsetY) * 48 + x + offsetX;
+        for (let run = 0; run < numRuns; ++run) {
+            const runSpec = dataView.getUint8(ptr++);
+            const runLength = bits(runSpec, 0, 6) + 1;
+            const type = bits(runSpec, 6, 2);
+            if (type == 2) {
+                const color = dataView.getUint8(ptr++);
+                for (let i = 0; i < runLength; ++i) {
+                    pixels[offset()] = color;
+                    x++;
+                }
+            }
+            else if (type == 1 || type == 3) {
+                for (let i = 0; i < runLength; ++i) {
+                    pixels[offset()] = dataView.getUint8(ptr++);
+                    x++;
+                }
+            }
+            else {
+                x += runLength;
+            }
+        }
+    }
+    return pixels;
+}
