75c190e5ce5825f8595210d46cf7c05579b6fd68
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-10-15 17:15:23 +0200

    Dynamic brick library texture size (doesn't fix the issue with some Zeelish grids having broken bricks)

--- src/iso/mapping.js
+++ src/iso/mapping.js
@@ -7,8 +7,6 @@ import {
 } from 'lodash';
 import THREE from 'three';
 
-const MAP_SIZE = 2048;
-
 export function loadBricksMapping(layouts, bricks, palette) {
     const usedBricks = filter(
         uniq(
@@ -19,7 +17,8 @@ export function loadBricksMapping(layouts, bricks, palette) {
         idx => idx != 0
     );
     const bricksMap = {};
-    const image_data = new Uint8Array(MAP_SIZE * MAP_SIZE * 4);
+    const {width, height} = computeTextureSize(usedBricks.length);
+    const image_data = new Uint8Array(width * height * 4);
     each(usedBricks, (brick, idx) => {
         const offsetX = (idx % 21) * 48;
         const offsetY = Math.round(idx / 21) * 38;
@@ -31,7 +30,7 @@ export function loadBricksMapping(layouts, bricks, palette) {
         for (let y = 0; y < 38; ++y) {
             for (let x = 0; x < 48; ++x) {
                 const src_i = y * 48 + (47 - x);
-                const tgt_i = (y + offsetY) * MAP_SIZE + x + offsetX;
+                const tgt_i = (y + offsetY) * width + x + offsetX;
 
                 image_data[tgt_i * 4] = palette[pixels[src_i] * 3];
                 image_data[tgt_i * 4 + 1] = palette[pixels[src_i] * 3 + 1];
@@ -42,8 +41,8 @@ export function loadBricksMapping(layouts, bricks, palette) {
     });
     const texture = new THREE.DataTexture(
         image_data,
-        MAP_SIZE,
-        MAP_SIZE,
+        width,
+        height,
         THREE.RGBAFormat,
         THREE.UnsignedByteType,
         THREE.UVMapping,
@@ -57,5 +56,19 @@ export function loadBricksMapping(layouts, bricks, palette) {
     return {
         texture: texture,
         bricksMap: bricksMap
+    };
+}
+
+function computeTextureSize(numBricks) {
+    const dim = [512, 512];
+    let idx = 1;
+    const count = () => Math.floor(dim[0] / 48) * Math.floor(dim[1] / 38);
+    while (count() < numBricks) {
+        dim[idx] *= 2;
+        idx = 1 - idx;
+    }
+    return {
+        width: dim[0],
+        height: dim[1]
     };
 }
