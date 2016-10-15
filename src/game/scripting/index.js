45bf711a560ba98b6c13b03f86b4b581f9d297f5
Alexandre Fontoura <xesfnet@gmail.com>
2016-10-15 22:35:04 +0100

    Fixed running moving script. WARNING: high CPU usage.

--- src/game/scripting/index.js
+++ src/game/scripting/index.js
@@ -22,18 +22,32 @@ export function initScriptState() {
 export function processLifeScript(actor) {
     const state = actor.scriptState;
     const script = actor.lifeScript;
-    do {
-        const opcode = script.getUint8(state.life.offset++, true);
-        // RUN OPCODE
-    } while(state.life.continue);
+    if (script.byteLength > 0 && state.life.offset >= 0) {
+        do {
+            const opcode = script.getUint8(state.life.offset++, true);
+            // RUN OPCODE
+        } while(state.life.continue);
+    }
 }
 
 export function processMoveScript(actor) {
     const state = actor.scriptState;
     const script = actor.moveScript;
     state.continue = true;
-    do {
-        const opcode = script.getUint8(state.move.offset++, true);
-        MOVE_OPCODE[opcode].callback(script, state.move, actor);
-    } while(state.move.continue);
+    if (script.byteLength > 0 && state.move.offset >= 0) {
+        while (state.move.continue) {
+            if (state.move.offset >= script.byteLength) {
+                console.warn("MoveScript error: offset > length");
+                state.move.offset = -1;
+                break;
+            }
+            const opcode = script.getUint8(state.move.offset++, true);
+            /*console.debug("opcode: " + opcode);
+            console.debug("state: ", state.move);
+            console.debug("opcode def: ", MOVE_OPCODE[opcode]);
+            console.debug(MOVE_OPCODE[opcode].command);*/
+            MOVE_OPCODE[opcode].callback(script, state.move, actor);
+            state.move.offset += MOVE_OPCODE[opcode].offset;
+        }
+    }
 }
