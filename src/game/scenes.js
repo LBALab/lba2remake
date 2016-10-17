0d906ed5ed8aeb32d27b74731dc29132ddd0b51c
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-10-17 23:19:54 +0200

    Load side scenes (don't animate side scene actors for now)

--- src/game/scenes.js
+++ src/game/scenes.js
@@ -1,8 +1,12 @@
 import async from 'async';
 import THREE from 'three';
-import {map, each, extend} from 'lodash';
+import {
+    map,
+    filter,
+    each
+} from 'lodash';
 
-import sceneMapping from '../island/data/sceneMapping';
+import islandSceneMapping from '../island/data/sceneMapping';
 import {loadIslandScenery} from '../island';
 import {loadIsometricScenery} from '../iso';
 import {loadSceneData} from '../scene';
@@ -23,7 +27,7 @@ export function createSceneManager(renderer, hero, callback) {
         callback({
             getScene: () => scene,
             goto: (index, debug = false) => {
-                loadScene(sceneMap, index, null, debug, (pScene) => {
+                loadScene(sceneMap, index, null, debug, (err, pScene) => {
                     hero.physics.position.x = pScene.scenery.props.startPosition[0];
                     hero.physics.position.z = pScene.scenery.props.startPosition[1];
                     renderer.applySceneryProps(pScene.scenery.props);
@@ -34,55 +38,91 @@ export function createSceneManager(renderer, hero, callback) {
     });
 }
 
-function loadScene(sceneMap, index, threeScene, debug, callback) {
+function loadScene(sceneMap, index, parent, debug, callback) {
     loadSceneData(index, sceneData => {
         const indexInfo = sceneMap[index];
+        const type = indexInfo.isIsland ? SceneryType.ISLAND : SceneryType.ISOMETRIC;
         const loadSteps = {
-            actors: callback => { async.map(sceneData.actors, loadActor, callback) },
-            points: callback => { async.map(sceneData.points, loadPoint, callback) },
-            zones: callback => { async.map(sceneData.zones, loadZone, callback) }
+            actors: (callback) => { async.map(sceneData.actors, loadActor, callback) },
+            points: (callback) => { async.map(sceneData.points, loadPoint, callback) },
+            zones: (callback) => { async.map(sceneData.zones, loadZone, callback) }
         };
 
-        if (!threeScene) {
-            threeScene = new THREE.Scene();
+        if (!parent) {
             loadSteps.scenery = indexInfo.isIsland
-                ? loadIslandScenery.bind(null, sceneMapping[indexInfo.index].island)
+                ? loadIslandScenery.bind(null, islandSceneMapping[index].island)
                 : loadIsometricScenery.bind(null, indexInfo.index);
+            loadSteps.threeScene = ['scenery', (data, callback) => {
+                const threeScene = new THREE.Scene();
+                threeScene.add(data.scenery.threeObject);
+                callback(null, threeScene);
+            }];
+            if (type == SceneryType.ISLAND) {
+                loadSteps.sideScenes = ['scenery', 'threeScene', (data, callback) => {
+                    loadSideScenes(sceneMap, index, data, debug, callback);
+                }];
+            }
+        } else {
+            loadSteps.scenery = (callback) => { callback(null, parent.scenery); };
+            loadSteps.threeScene = (callback) => { callback(null, parent.threeScene); };
         }
 
         async.auto(loadSteps, function (err, data) {
-            const sceneNode = loadSceneNode(indexInfo, data);
-            const addToSceneNode = obj => {
-                sceneNode.add(obj.threeObject);
-            };
-            threeScene.add(data.scenery.threeObject);
-            threeScene.add(sceneNode);
-            each(data.actors, addToSceneNode);
-            if (debug) {
-                each(data.zones, addToSceneNode);
-                each(data.points, addToSceneNode);
-            }
-            callback(extend({
+            const sceneNode = loadSceneNode(index, indexInfo, data, debug);
+            data.threeScene.add(sceneNode);
+            callback(null, {
                 index: index,
-                type: indexInfo.isIsland ? SceneryType.ISLAND : SceneryType.ISOMETRIC,
-                threeScene: threeScene,
+                type: type,
+                threeScene: data.threeScene,
+                scenery: data.scenery,
                 update: time => {
                     each(data.actors, actor => {
                         actor.update(time);
                     });
                 }
-            }, data));
+            });
         });
     });
 }
 
-function loadSceneNode(indexInfo, data) {
+function loadSceneNode(index, indexInfo, data, debug) {
     const sceneNode = new THREE.Object3D();
     if (indexInfo.isIsland) {
-        const sectionIdx = sceneMapping[indexInfo.index].section;
+        const sectionIdx = islandSceneMapping[index].section;
         const section = data.scenery.sections[sectionIdx];
         sceneNode.position.x = section.x * 2;
         sceneNode.position.z = section.z * 2;
     }
+    const addToSceneNode = obj => {
+        sceneNode.add(obj.threeObject);
+    };
+
+    each(data.actors, addToSceneNode);
+    if (debug) {
+        each(data.zones, addToSceneNode);
+        each(data.points, addToSceneNode);
+    }
     return sceneNode;
+}
+
+function loadSideScenes(sceneMap, index, parent, debug, callback) {
+    const sideIndices = filter(
+        map(sceneMap, (indexInfo, sideIndex) => {
+            if (sideIndex != index && indexInfo.isIsland && sideIndex in islandSceneMapping) {
+                if (islandSceneMapping[sideIndex].island == islandSceneMapping[index].island) {
+                    return sideIndex;
+                }
+            }
+        }),
+        id => id != null
+    );
+    async.map(sideIndices, (sideIndex, callback) => {
+        loadScene(sceneMap, sideIndex, parent, debug, callback);
+    }, (err, sideScenes) => {
+        const sideScenesMap = {};
+        each(sideScenes, sideScene => {
+            sideScenesMap[sideScene.index] = sideScene;
+        });
+        callback(null, sideScenesMap);
+    });
 }
--- src/island/data/sceneMapping.js
+++ src/island/data/sceneMapping.js
@@ -1,11 +1,11 @@
 export default {
-    0: {island: 'CITABAU', section: 6},
-    1: {island: 'CITABAU', section: 5},
-    2: {island: 'CITABAU', section: 3},
-    3: {island: 'CITABAU', section: 0},
-    4: {island: 'CITABAU', section: 7},
-    5: {island: 'CITABAU', section: 1},
-    6: {island: 'CITABAU', section: 4},
-    7: {island: 'CITABAU', section: 8},
-    8: {island: 'CITABAU', section: 2}
+    42: {island: 'CITABAU', section: 6},
+    43: {island: 'CITABAU', section: 5},
+    44: {island: 'CITABAU', section: 3},
+    45: {island: 'CITABAU', section: 0},
+    46: {island: 'CITABAU', section: 7},
+    47: {island: 'CITABAU', section: 1},
+    48: {island: 'CITABAU', section: 4},
+    49: {island: 'CITABAU', section: 8},
+    50: {island: 'CITABAU', section: 2}
 };
