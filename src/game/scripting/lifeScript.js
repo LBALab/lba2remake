3c15fb61c4aab1d3f3f277bc56b89d315418cbe3
Alexandre Fontoura <xesfnet@gmail.com>
2016-10-19 21:32:41 +0100

    Adding condition opcode

--- src/game/scripting/lifeScript.js
+++ src/game/scripting/lifeScript.js
@@ -168,6 +168,56 @@ export const OperatorOpcode = [
 ];
 
 
+export const ConditionOpcode = [
+    { opcode: 0x00, command: "COL", param: 0, value_size: 1 },
+    { opcode: 0x01, command: "COL_OBJ", param: 1, value_size: 1 },
+    { opcode: 0x02, command: "DISTANCE", param: 1, value_size: 2 },
+    { opcode: 0x03, command: "ZONE", param: 0, value_size: 1 },
+    { opcode: 0x04, command: "ZONE_OBJ", param: 1, value_size: 1 },
+    { opcode: 0x05, command: "BODY", param: 0, value_size: 1 },
+    { opcode: 0x06, command: "BODY_OBJ", param: 1, value_size: 1 },
+    { opcode: 0x07, command: "ANIM", param: 0, value_size: 2 },
+    { opcode: 0x08, command: "ANIM_OBJ", param: 1, value_size: 2 },
+    { opcode: 0x09, command: "CURRENT_TRACK", param: 0, value_size: 1 },
+    { opcode: 0x0A, command: "CURRENT_TRACK_OBJ", param: 1, value_size: 1 },
+    { opcode: 0x0B, command: "VAR_CUBE", param: 1, value_size: 1 },
+    { opcode: 0x0C, command: "CONE_VIEW", param: 1, value_size: 2 },
+    { opcode: 0x0D, command: "HIT_BY", param: 0, value_size: 1 },
+    { opcode: 0x0E, command: "ACTION", param: 0, value_size: 1 },
+    { opcode: 0x0F, command: "VAR_GAME", param: 1, value_size: 2 },
+    { opcode: 0x10, command: "LIFE_POINT", param: 0, value_size: 2 },
+    { opcode: 0x11, command: "LIFE_POINT_OBJ", param: 1, value_size: 2 },
+    { opcode: 0x12, command: "NUM_LITTLE_KEYS", param: 0, value_size: 1 },
+    { opcode: 0x13, command: "NUM_GOLD_PIECES", param: 0, value_size: 2 },
+    { opcode: 0x14, command: "BEHAVIOUR", param: 0, value_size: 1 },
+    { opcode: 0x15, command: "CHAPTER", param: 0, value_size: 1 },
+    { opcode: 0x16, command: "DISTANCE_3D", param: 1, value_size: 1 },
+    { opcode: 0x17, command: "MAGIC_LEVEL", param: 0, value_size: 1 },
+    { opcode: 0x18, command: "MAGIC_POINT", param: 0, value_size: 1 },
+    { opcode: 0x19, command: "USE_INVENTORY", param: 1, value_size: 1 },
+    { opcode: 0x1A, command: "CHOICE", param: 0, value_size: 2 },
+    { opcode: 0x1B, command: "FUEL", param: 0, value_size: 1 },
+    { opcode: 0x1C, command: "CARRIED_BY", param: 0, value_size: 1 },
+    { opcode: 0x1D, command: "CDROM", param: 0, value_size: 1 },
+    { opcode: 0x1E, command: "LADDER", param: 0, value_size: 1 },
+    { opcode: 0x1F, command: "RND", param: 1, value_size: 1 },
+    { opcode: 0x20, command: "RAIL", param: 1, value_size: 1 },
+    { opcode: 0x21, command: "BETA", param: 0, value_size: 2 },
+    { opcode: 0x22, command: "BETA_OBJ", param: 1, value_size: 2 },
+    { opcode: 0x23, command: "CARRIED_OBJ_BY", param: 1, value_size: 1 },
+    { opcode: 0x24, command: "ANGLE", param: 1, value_size: 2 },
+    { opcode: 0x25, command: "DISTANCE_MESSAGE", param: 1, value_size: 2 },
+    { opcode: 0x26, command: "HIT_OBJ_BY", param: 1, value_size: 1 },
+    { opcode: 0x27, command: "REAL_ANGLE", param: 0, value_size: 2 },
+    { opcode: 0x28, command: "DEMO", param: 0, value_size: 1 },
+    { opcode: 0x29, command: "COL_DECORS", param: 0, value_size: 1 },
+    { opcode: 0x2A, command: "COL_DECORS_OBJ", param: 1, value_size: 1 },
+    { opcode: 0x2B, command: "PROCESSOR", param: 0, value_size: 1 },
+    { opcode: 0x2C, command: "OBJECT_DISPLAYED", param: 0, value_size: 1 },
+    { opcode: 0x2D, command: "ANGLE_OBJ", param: 0, value_size: 1 }
+];
+
+
 function lsEND(script, state, actor) {
 
 }
