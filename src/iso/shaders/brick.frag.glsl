6a9a331485b22f3f2278ab39b85767c1f903ead1
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-10-20 22:40:19 +0200

    Asjust projection a bit, simplify shader code

--- src/iso/shaders/brick.frag.glsl
+++ src/iso/shaders/brick.frag.glsl
@@ -7,7 +7,7 @@ varying vec2 vCenter;
 varying vec2 vTile;
 
 void main() {
-    vec2 offset = (vCenter - gl_FragCoord.xy);
-    vec2 nOffset = (offset + vec2(24.0, 19.0)) / vec2(48.0, 38.0);
+    vec2 offset = floor(vCenter - gl_FragCoord.xy);
+    vec2 nOffset = offset / vec2(48.0, 38.0) + 0.5;
     gl_FragColor = vec4(texture2D(library, vTile + nOffset * tileSize));
 }
--- src/renderer/cameras.js
+++ src/renderer/cameras.js
@@ -1,14 +1,16 @@
 import THREE from 'three';
 
 export function getIsometricCamera() {
-    let v = 1088;
+    let v = 1086;
     const halfWidth = Math.floor(window.innerWidth / 2) / v;
     const halfHeight = Math.floor(window.innerHeight / 2) / v;
     const camera = new THREE.OrthographicCamera(-halfWidth, halfWidth, halfHeight, -halfHeight, 0.1, 20); // 1m = 0.0625 units
 
+    let y = 8.145;
+
     const lookAt = new THREE.Vector3(1.94875, 0, 0.27);
     camera.position.copy(lookAt);
-    camera.position.add(new THREE.Vector3(-10, 8.1, 10));
+    camera.position.add(new THREE.Vector3(-10, y, 10));
     camera.lookAt(lookAt);
 
     const mv = 0.005;
@@ -36,18 +38,24 @@ export function getIsometricCamera() {
                 update = true;
                 break;
             case 'NumpadAdd':
-                v += 1;
+                //v += 1;
+                y += 0.001;
+                camera.position.copy(lookAt);
+                camera.position.add(new THREE.Vector3(-10, y, 10));
                 update = true;
                 break;
             case 'NumpadSubtract':
-                v -= 1;
+                //v -= 1;
+                y -= 0.001;
+                camera.position.copy(lookAt);
+                camera.position.add(new THREE.Vector3(-10, y, 10));
                 update = true;
                 break;
         }
         if (update) {
             camera.lookAt(lookAt);
             resizeIsometricCamera(camera, v);
-            console.log(lookAt, v);
+            console.log(lookAt, v, y);
         }
     }, false);
 
@@ -55,7 +63,7 @@ export function getIsometricCamera() {
 }
 
 export function resizeIsometricCamera(camera, v) {
-    v = v || 1088;
+    v = v || 1086;
     const halfWidth = Math.floor(window.innerWidth / 2) / v;
     const halfHeight = Math.floor(window.innerHeight / 2) / v;
     camera.left = -halfWidth;
