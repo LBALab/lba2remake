5f6e5af3b0693b93398c03f2c8c777bf4df0b850
Adrien Grandemange <adrien.grandemange@gmail.com>
2016-10-20 23:36:22 +0200

    Clamp to remove some of the bleeding (not all)

--- src/iso/shaders/brick.frag.glsl
+++ src/iso/shaders/brick.frag.glsl
@@ -8,6 +8,6 @@ varying vec2 vTile;
 
 void main() {
     vec2 offset = floor(vCenter - gl_FragCoord.xy);
-    vec2 nOffset = offset / vec2(48.0, 38.0) + 0.5;
+    vec2 nOffset = clamp(offset / vec2(48.0, 38.0) + 0.5, vec2(0.0), vec2(1.0));
     gl_FragColor = vec4(texture2D(library, vTile + nOffset * tileSize));
 }
