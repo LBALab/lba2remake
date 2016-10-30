d6ba67b85e22ba2679d177e36fd2bfe9fc62fb6c
Alexandre Fontoura <xesfnet@gmail.com>
2016-10-30 22:39:30 +0000

    removing debug flags

--- src/debugFlags.js
+++ src/debugFlags.js
@@ -1,4 +1,4 @@
 export const DISPLAY_ZONES = true;
 export const DISPLAY_POINTS = true;
-export const DISPLAY_SCRIPT_LINE = true;
-export const DISPLAY_SCRIPT_MOVE = true;
+export const DISPLAY_SCRIPT_LINE = false;
+export const DISPLAY_SCRIPT_MOVE = false;
