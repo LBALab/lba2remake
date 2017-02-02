68515f80743b6fc2e260ed887f4068616363042a
Adrien Grandemange <adrien.grandemange@gmail.com>
2017-02-02 01:04:51 +0100

    Fill out the debug panel

--- tools/script_dbg/devtools.js
+++ tools/script_dbg/devtools.js
@@ -1,7 +1,1 @@
-chrome.devtools.panels.create("LBA2 Scripts",
-    "icon.png",
-    "panel.html",
-    function(panel) {
-        // code invoked on panel creation
-    }
-);
+chrome.devtools.panels.create("LBA2 Scripts", "icon.png", "panel.html", function(panel) {});
