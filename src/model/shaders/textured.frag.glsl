12074d1800f1c4ae95395799135f4009c378d4b8
Adrien Grandemange <adrien.grandemange@gmail.com>
2017-02-16 01:20:12 +0100

    Comment unused shader variables to avoid warning

--- src/model/shaders/textured.frag.glsl
+++ src/model/shaders/textured.frag.glsl
@@ -9,9 +9,9 @@ precision highp float;
 uniform sampler2D texture;
 uniform vec3 light;
 
-varying vec3 vPosition;
-varying vec3 vNormal;
-varying float vColor;
+//varying vec3 vPosition;
+//varying vec3 vNormal;
+//varying float vColor;
 varying vec2 vUv;
 
 #require "../../island/shaders/common/fog.frag"
--- src/model/shaders/textured.vert.glsl
+++ src/model/shaders/textured.vert.glsl
@@ -11,16 +11,16 @@ attribute vec2 uv;
 attribute float color;
 attribute float boneIndex;
 
-varying vec3 vPosition;
-varying vec3 vNormal;
-varying float vColor;
+//varying vec3 vPosition;
+//varying vec3 vNormal;
+//varying float vColor;
 varying vec2 vUv;
 
 void main() {
     gl_Position = projectionMatrix * modelViewMatrix * bones[int(boneIndex)] * vec4(position, 1.0);
-    vPosition = position;
-    vec4 newNormal = rotationMatrix * bones[int(boneIndex)] * vec4(normal, 1.0);
-    vNormal = newNormal.xyz;
-    vColor = color;
+    //vPosition = position;
+    //vec4 newNormal = rotationMatrix * bones[int(boneIndex)] * vec4(normal, 1.0);
+    //vNormal = newNormal.xyz;
+    //vColor = color;
     vUv = uv;
 }
