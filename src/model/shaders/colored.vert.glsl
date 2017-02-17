fa317917723b17eb5d64dc0f236e769c1a99e83c
Adrien Grandemange <adrien.grandemange@gmail.com>
2017-02-17 21:22:30 +0100

    Compute depth

--- src/model/shaders/colored.vert.glsl
+++ src/model/shaders/colored.vert.glsl
@@ -19,10 +19,10 @@ vec4 isoProjection(vec4 basePosition, vec2 scale) {
     mat4 scaleM = mat4(32.0, 0.0, 0.0, 0.0, 0.0, 32.0, 0.0, 0.0, 0.0, 0.0, 32.0, 0.0, 0.0, 0.0, 0.0, 1.0);
     mat4 rotation = mat4(0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
     mat4 projection = mat4(
-        48.0 / scale.x    , -24.0 / scale.y   , 0.0, 0.0,
-        0.0               , 60.0 / scale.y    , 0.0, 0.0,
-        -48.0 / scale.x   , -24.0 / scale.y   , 0.0, 0.0,
-        -3500.0 / scale.x , -1000.0 / scale.y , 0.5, 1.0
+        48.0 / scale.x    , -24.0 / scale.y   , -0.01, 0.0,
+        0.0               , 60.0 / scale.y    , -0.01, 0.0,
+        -48.0 / scale.x   , -24.0 / scale.y   , -0.01, 0.0,
+        -3500.0 / scale.x , -1000.0 / scale.y , 0.0, 1.0
     );
     return projection * scaleM * rotation * modelMatrix * basePosition;
 }
