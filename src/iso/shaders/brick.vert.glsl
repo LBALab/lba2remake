11480b596cd3931937b848abbed9cfcc4c72a8e1
Adrien Grandemange <adrien.grandemange@gmail.com>
2017-02-17 21:17:04 +0100

    Use projection matrix instead of manual calculations (in preparation for isometric camera implementation)

--- src/iso/shaders/brick.vert.glsl
+++ src/iso/shaders/brick.vert.glsl
@@ -13,13 +13,13 @@ varying vec2 vTile;
 vec4 isoProjection(vec4 basePosition, vec2 scale) {
     mat4 scaleM = mat4(32.0, 0.0, 0.0, 0.0, 0.0, 32.0, 0.0, 0.0, 0.0, 0.0, 32.0, 0.0, 0.0, 0.0, 0.0, 1.0);
     mat4 rotation = mat4(0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
-    vec4 pos = scaleM * rotation * modelMatrix  * basePosition;
-    return vec4(
-        ((pos.x - pos.z) * 48.0 - 3500.0) / scale.x,
-        -((pos.x + pos.z) * 24.0 - pos.y * 60.0 + 1000.0) / scale.y,
-        1.0,
-        1.0
+    mat4 projection = mat4(
+        48.0 / scale.x    , -24.0 / scale.y   , 0.0, 0.0,
+        0.0               , 60.0 / scale.y    , 0.0, 0.0,
+        -48.0 / scale.x   , -24.0 / scale.y   , 0.0, 0.0,
+        -3500.0 / scale.x , -1000.0 / scale.y , 1.0, 1.0
     );
+    return projection * scaleM * rotation * modelMatrix * basePosition;
 }
 
 void main() {
