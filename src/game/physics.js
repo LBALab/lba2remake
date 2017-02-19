c266d919226457085b6917ec1eedd567114c3313
Adrien Grandemange <adrien.grandemange@gmail.com>
2017-02-19 18:30:07 +0100

    Use movement animation

--- src/game/physics.js
+++ src/game/physics.js
@@ -4,24 +4,32 @@ export function processPhysicsFrame(game, scene, time) {
     processHeroMovement(game.controlsState, scene, time);
 }
 
-const orientedSpeed = new THREE.Vector3();
 const euler = new THREE.Euler();
-const q = new THREE.Quaternion();
 
 function processHeroMovement(controlsState, scene, time) {
     const actor = scene.getActor(0);
+    let animIndex = actor.props.animIndex;
+    if (controlsState.heroSpeed != 0) {
+        actor.isWalking = true;
+        animIndex = controlsState.heroSpeed == 1 ? 1 : 2;
+    } else {
+        actor.isWalking = false;
+        animIndex = 0;
+    }
     if (controlsState.heroRotationSpeed != 0) {
         euler.setFromQuaternion(actor.physics.orientation, 'YXZ');
         euler.y += controlsState.heroRotationSpeed * time.delta * 1.2;
+        actor.physics.temp.angle = euler.y;
+        if (controlsState.heroSpeed == 0) {
+            animIndex = controlsState.heroRotationSpeed == 1 ? 3 : 4;
+        }
         actor.physics.orientation.setFromEuler(euler);
     }
-    if (controlsState.heroSpeed != 0) {
-        orientedSpeed.set(0, 0, controlsState.heroSpeed * 0.05);
-        orientedSpeed.applyQuaternion(actor.physics.orientation);
-        orientedSpeed.multiplyScalar(time.delta);
-        actor.physics.position.add(orientedSpeed);
-        //actor.physics.position.y = scene.scenery.physics.getGroundHeight(actor.physics.position.x, actor.physics.position.z);
+    if (actor.props.animIndex != animIndex) {
+        actor.props.animIndex = animIndex;
+        actor.resetAnimState();
     }
+
 }
 
 
