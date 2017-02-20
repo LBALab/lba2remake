1a085b2c9d795fb7a7daee298ab257879beb1ad2
Alexandre Fontoura <xesfnet@gmail.com>
2017-02-20 19:52:08 +0000

    Wave music data. It will be used to play CD tracks and scene musics.

---
+++ src/audio/data.js
@@ -0,0 +1,32 @@
+export default {
+    LOGO: { file:'MUSIC/LOGADPCM.WAV' },
+    TRACK: [
+        { index: 0, file:null },
+        { index: 1, file:'MUSIC/TADPCM1.WAV' },
+        { index: 2, file:'MUSIC/TADPCM2.WAV' },
+        { index: 3, file:'MUSIC/TADPCM3.WAV' },
+        { index: 4, file:'MUSIC/TADPCM4.WAV' },
+        { index: 5, file:'MUSIC/TADPCM5.WAV' }
+    ],
+    MUSIC: [
+        { index: 0, file:null },
+        { index: 1, file:'MUSIC/JADPCM01.WAV' },
+        { index: 2, file:'MUSIC/JADPCM02.WAV' },
+        { index: 3, file:'MUSIC/JADPCM03.WAV' },
+        { index: 4, file:'MUSIC/JADPCM04.WAV' },
+        { index: 5, file:'MUSIC/JADPCM05.WAV' },
+        { index: 6, file:'MUSIC/JADPCM06.WAV' },
+        { index: 7, file:'MUSIC/JADPCM07.WAV' },
+        { index: 8, file:'MUSIC/JADPCM08.WAV' },
+        { index: 9, file:'MUSIC/JADPCM09.WAV' },
+        { index: 10, file:'MUSIC/JADPCM10.WAV' },
+        { index: 11, file:'MUSIC/JADPCM11.WAV' },
+        { index: 12, file:'MUSIC/JADPCM12.WAV' },
+        { index: 13, file:'MUSIC/JADPCM13.WAV' },
+        { index: 14, file:'MUSIC/JADPCM14.WAV' },
+        { index: 15, file:'MUSIC/JADPCM15.WAV' },
+        { index: 16, file:'MUSIC/JADPCM16.WAV' },
+        { index: 17, file:'MUSIC/JADPCM17.WAV' },
+        { index: 18, file:'MUSIC/JADPCM18.WAV' },
+    ]
+}
