e2292c63bb7dcf05246023ba0c01efbc28faca5c
Alexandre Fontoura <xesfnet@gmail.com>
2017-02-20 19:51:37 +0000

    First audio manager structure with multiple sources. For now 1 for music, another for sfx.

--- src/audio/index.js
+++ src/audio/index.js
@@ -1,13 +1,51 @@
 
-import {createAudioContext} from './api';
-
 export function getAudioManager() {
     const audio = {
         context: createAudioContext(),
-        play: () => {},
-        stop: () => {},
-        pause: () => {}
+        getMusicSource: () => getAudioSource(this.context),
+        getSfxSource: () => getAudioSource(this.context)
     };
 
     return audio;
+}
+
+function getAudioSource(context) {
+    const source = {
+        volume: 1,
+        bufferSource: context.createBufferSource(),
+        gainNode: context.createGain(),
+        play: () => { this.source.start(); },
+        stop: () => { this.source.stop(); },
+        pause: () => {},
+        load: (file, callback) => {
+            loadAudioAsync(file, function (buffer) {
+                this.source.buffer = buffer;
+                this.source.connect(this.gainNode);
+                this.gainNode.gain.value = this.volume;
+                this.gainNode.connect(context.destination);
+                callback.call();
+            });
+        }
+    };
+
+    return source;
+}
+
+function createAudioContext() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext; // needed for Safari
+    return new AudioContext();
+}
+
+function loadAudioAsync(context, url, callback) {
+    const request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+
+    request.onload = function() {
+        context.decodeAudioData(request.response, callback, function(err) {
+            throw new Error(err);
+        });
+    };
+
+    request.send();
 }
