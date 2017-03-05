20920afb5a18734fcf4189f1cc905528a649ec5f
Adrien Grandemange <adrien.grandemange@gmail.com>
2017-03-06 00:27:39 +0100

    Factoring some code

--- src/island/index.js
+++ src/island/index.js
@@ -9,6 +9,7 @@ import {loadGround} from './ground';
 import {loadSea} from './sea';
 import {loadObjects} from './objects';
 import {loadIslandPhysics} from './physics';
+import {createBoundingBox} from '../utils/rendering';
 
 import islandsInfo from './data/islands';
 import environments from './data/environments';
@@ -81,7 +82,7 @@ function loadIslandNode(props, files, ambience) {
 
     each(data.layout.groundSections, section => {
         each(section.boundingBoxes, bb => {
-            islandObject.add(makeBB(bb));
+            islandObject.add(createBoundingBox(bb, new THREE.Vector3(1, 0, 0)));
         });
     });
 
@@ -96,44 +97,6 @@ function loadIslandNode(props, files, ambience) {
         physics: loadIslandPhysics(layout),
         update: time => { seaTimeUniform.value = time.elapsed; }
     };
-}
-
-function makeBB(bb) {
-    const geometry = new THREE.BoxGeometry(
-        bb.max.x - bb.min.x,
-        bb.max.y - bb.min.y,
-        bb.max.z - bb.min.z
-    );
-
-    const edgesGeometry = new THREE.EdgesGeometry(geometry);
-    const material = new THREE.RawShaderMaterial({
-        vertexShader: `
-            precision highp float;
-        
-            uniform mat4 projectionMatrix;
-            uniform mat4 modelViewMatrix;
-            
-            attribute vec3 position;
-            
-            void main() {
-                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-                gl_Position.z = 0.0;
-            }`,
-        fragmentShader: `
-            precision highp float;
-            
-            uniform vec3 color;
-            
-            void main() {
-                gl_FragColor = vec4(color, 1.0);
-            }`,
-        uniforms: {
-            color: {value: new THREE.Vector3(1, 0, 0)}
-        }
-    });
-    const wireframe = new THREE.LineSegments(edgesGeometry, material);
-    wireframe.position.copy(bb.center());
-    return wireframe;
 }
 
 function loadSky(geometries) {
---
+++ src/utils/rendering.js
@@ -0,0 +1,39 @@
+import THREE from 'three';
+
+export function createBoundingBox(bb, color) {
+    const geometry = new THREE.BoxGeometry(
+        bb.max.x - bb.min.x,
+        bb.max.y - bb.min.y,
+        bb.max.z - bb.min.z
+    );
+
+    const edgesGeometry = new THREE.EdgesGeometry(geometry);
+    const material = new THREE.RawShaderMaterial({
+        vertexShader: `
+            precision highp float;
+        
+            uniform mat4 projectionMatrix;
+            uniform mat4 modelViewMatrix;
+            
+            attribute vec3 position;
+            
+            void main() {
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+                gl_Position.z = 0.0;
+            }`,
+        fragmentShader: `
+            precision highp float;
+            
+            uniform vec3 color;
+            
+            void main() {
+                gl_FragColor = vec4(color, 1.0);
+            }`,
+        uniforms: {
+            color: {value: color}
+        }
+    });
+    const wireframe = new THREE.LineSegments(edgesGeometry, material);
+    wireframe.position.copy(bb.center());
+    return wireframe;
+}
