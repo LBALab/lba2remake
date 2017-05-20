ec2cb62194cd23a72c0708f80b5ed347ffa60d09
Alexandre Fontoura <xesfnet@gmail.com>
2017-05-20 08:25:37 +0100

    Testing camera animation using Tween library. Need to check if has performance impacts but I guess as a first try it seems great and easy to use. Anyway, a possible custom version will have to be done like we do in the animations. This has been added into the lba2remake server so that Fred can test.

--- src/game/loop/cameras/3d.js
+++ src/game/loop/cameras/3d.js
@@ -1,7 +1,8 @@
 import THREE from 'three';
+import { Easing, Interpolation, Tween, update, autoPlay } from 'es6-tween';
 
 export function processFollow3DMovement(controlsState, camera, scene, time) {
-    const hero = scene.getActor(0);
+    /*const hero = scene.getActor(0);
     const heroPos = new THREE.Vector3(0, 0.08, 0);
     heroPos.applyMatrix4(hero.threeObject.matrixWorld);
     camera.position.copy(heroPos);
@@ -10,7 +11,41 @@ export function processFollow3DMovement(controlsState, camera, scene, time) {
     camera.position.add(offset);
     camera.lookAt(heroPos);
     controlsState.cameraOrientation.copy(hero.threeObject.quaternion);
-    controlsState.cameraOrientation.multiply(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI));
+    controlsState.cameraOrientation.multiply(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI));*/
+
+    const hero = scene.getActor(0);
+    const heroPos = new THREE.Vector3(0, 0.08, 0);
+    heroPos.applyMatrix4(hero.threeObject.matrixWorld);
+
+    const cameraPos = new THREE.Vector3(0, 0.15, -0.2) ;
+    cameraPos.applyMatrix4(hero.threeObject.matrixWorld);
+
+    const from = {
+        x: camera.position.x,
+        y: camera.position.y,
+        z: camera.position.z
+    };
+
+    const to = {
+        x: cameraPos.x,
+        y: cameraPos.y,
+        z: cameraPos.z
+    };
+
+    autoPlay(true);
+
+    new Tween(from).to(to, 150)
+        .interpolation(Interpolation.Bezier)
+        .easing(Easing.Sinusoidal.InOut)
+        .on('update', (obj) => {
+            camera.position.set(obj.x, obj.y, obj.z);
+            })
+        .on('complete', () => {
+            camera.position.copy(cameraPos);
+        })
+        .start();
+
+    camera.lookAt(heroPos);
 }
 
 export function processFree3DMovement(controlsState, camera, scene, time) {
