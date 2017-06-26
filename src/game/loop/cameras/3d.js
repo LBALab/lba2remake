45955662fae05d16a2f272f401174866d4a90371
Alexandre Fontoura <xesfnet@gmail.com>
2017-06-27 00:52:33 +0100

    Getting rid of the Tween library.

--- src/game/loop/cameras/3d.js
+++ src/game/loop/cameras/3d.js
@@ -1,50 +1,18 @@
 import THREE from 'three';
-import { Easing, Interpolation, Tween, update, autoPlay } from 'es6-tween';
 
 export function processFollow3DMovement(controlsState, camera, scene, time) {
-    /*const hero = scene.getActor(0);
-    const heroPos = new THREE.Vector3(0, 0.08, 0);
-    heroPos.applyMatrix4(hero.threeObject.matrixWorld);
-    camera.position.copy(heroPos);
-    const offset = new THREE.Vector3(0, 0.1, -0.3);
-    offset.applyQuaternion(hero.threeObject.quaternion);
-    camera.position.add(offset);
-    camera.lookAt(heroPos);
-    controlsState.cameraOrientation.copy(hero.threeObject.quaternion);
-    controlsState.cameraOrientation.multiply(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI));*/
-
     const hero = scene.getActor(0);
     const heroPos = new THREE.Vector3(0, 0.08, 0);
     heroPos.applyMatrix4(hero.threeObject.matrixWorld);
 
     const cameraPos = new THREE.Vector3(0, 0.15, -0.2) ;
-    cameraPos.applyMatrix4(hero.threeObject.matrixWorld);
+    cameraPos.applyMatrix4(hero.threeObject.matrixWorld) ;
 
-    const from = {
-        x: camera.position.x,
-        y: camera.position.y,
-        z: camera.position.z
-    };
-
-    const to = {
-        x: cameraPos.x,
-        y: cameraPos.y,
-        z: cameraPos.z
-    };
-
-    autoPlay(true);
-
-    new Tween(from).to(to, 150)
-        .interpolation(Interpolation.Bezier)
-        .easing(Easing.Sinusoidal.InOut)
-        .on('update', (obj) => {
-            camera.position.set(obj.x, obj.y, obj.z);
-            })
-        .on('complete', () => {
-            camera.position.copy(cameraPos);
-        })
-        .start();
-
+    controlsState.cameraLerp.lerpVectors(camera.position, cameraPos, 0.025);
+    camera.position.set(controlsState.cameraLerp.x, controlsState.cameraLerp.y, controlsState.cameraLerp.z);
+    if (camera.position.distanceTo(cameraPos) > 1) {
+        camera.position.copy(cameraPos);
+    }
     camera.lookAt(heroPos);
 }
 
