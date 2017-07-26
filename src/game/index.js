39821e204984b65755e2c843f8c6d591561f4cd8
Alexandre Fontoura <xesfnet@gmail.com>
2017-07-26 22:55:01 +0100

    Adding free camera on touch for Mobile. Experimental yet

--- src/game/index.js
+++ src/game/index.js
@@ -9,6 +9,7 @@ import {makeFirstPersonMouseControls} from '../controls/mouse';
 import {makeKeyboardControls} from '../controls/keyboard';
 import {makeGyroscopeControls} from '../controls/gyroscope';
 import {makeGamepadControls} from '../controls/gamepad';
+import {makeFirstPersonTouchControls} from '../controls/touch';
 
 import {mainGameLoop} from './loop';
 import {createSceneManager} from './scenes';
@@ -110,6 +111,7 @@ export function createGame(params: Object, isMobile: boolean, callback : Functio
         }
         else if (isMobile) {
             controls = [
+                makeFirstPersonTouchControls(game),
                 makeGamepadControls(game)
             ];
         } else {
