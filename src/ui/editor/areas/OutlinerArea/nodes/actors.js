9dcb75257d642dd18ec1f7387fd720ebebe9bcbf
Adrien Grandemange <adrien.grandemange@gmail.com>
2017-10-17 01:07:06 +0200

    Display active behaviour of actor

--- src/ui/editor/areas/OutlinerArea/nodes/actors.js
+++ src/ui/editor/areas/OutlinerArea/nodes/actors.js
@@ -3,6 +3,17 @@ import DebugData from '../../../DebugData';
 
 const actorName = (idx) => idx === 0 ? 'hero' : `actor_${idx}`;
 
+function getComportement(actor) {
+    const lifeScript = actor.scripts.life;
+    if (lifeScript.context.state.terminated)
+        return 'terminated';
+    const offset = lifeScript.context.state.lastOffset;
+    if (offset) {
+        const cmd = lifeScript.commands[offset];
+        return cmd.section;
+    }
+}
+
 function getTarget(actor) {
     const moveScript = actor.scripts.move;
     const offset = moveScript.context.state.lastOffset;
@@ -28,7 +39,8 @@ export const ActorsNode = {
             return value.name !== actorName(idx)
                 || value.props[0].value !== actor.isVisible
                 || value.props[1].value !== actor.isSprite
-                || value.props[2].value !== getTarget(actor)
+                || value.props[2].value !== getComportement(actor)
+                || value.props[3].value !== getTarget(actor)
                 || value.selected !== (DebugData.selection.actor === idx);
         }
         return true;
@@ -42,15 +54,34 @@ export const ActorsNode = {
                 props: [
                     {id: 'visible', value: actor.isVisible},
                     {id: 'sprite', value: actor.isSprite},
+                    {id: 'comportement', value: getComportement(actor)},
                     {id: 'target', value: getTarget(actor)}
                 ],
                 renderProp: (id, value) => {
-                    if (id === 'visible')
+                    if (id === 'visible') {
                         return <img src={`editor/icons/${value ? 'visible' : 'hidden'}.png`}/>;
-                    else if (id === 'sprite')
-                        return <img title={value ? 'SPRITE' : 'MODEL'} src={`editor/icons/${value ? 'sprite' : 'model'}.png`}/>;
-                    else if (id === 'target')
-                        return value ? <span>=&gt; Pt.{value}</span> : '';
+                    }
+                    else if (id === 'sprite') {
+                        return <img title={value ? 'SPRITE' : 'MODEL'}
+                                    src={`editor/icons/${value ? 'sprite' : 'model'}.png`}/>;
+                    }
+                    else if (id === 'comportement') {
+                        const style = {
+                            border: '1px solid black',
+                            background: '#20a2ff',
+                            color: 'black',
+                            padding: '0 2px'
+                        };
+                        if (value === 'terminated') {
+                            style.background = '#680000';
+                            return <span style={style}>&nbsp;</span>;
+                        } else {
+                            return <span style={style}>{value}</span>;
+                        }
+                    }
+                    else if (id === 'target') {
+                        return value ? <span>=&gt; point_{value}</span> : '';
+                    }
                 },
                 selected: DebugData.selection.actor === idx,
                 onClick: () => {DebugData.selection.actor = idx},
