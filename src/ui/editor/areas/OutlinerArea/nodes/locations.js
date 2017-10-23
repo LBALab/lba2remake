d49c3dc740056b8e6dd86ba887e61ce684c5bac7
Adrien Grandemange <adrien.grandemange@gmail.com>
2017-10-23 21:48:50 +0200

    Mapped all scenes

--- src/ui/editor/areas/OutlinerArea/nodes/locations.js
+++ src/ui/editor/areas/OutlinerArea/nodes/locations.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DebugData from '../../../DebugData';
-import {extend} from 'lodash';
+import {each} from 'lodash';
 
 function goto(index) {
     if (DebugData.sceneManager) {
@@ -65,8 +65,12 @@ export const LocationsNode = {
                         {
                             name: 'Buildings',
                             children: [
-                                makeScene(0, 'Twinsen\'s House'),
-                                makeScene(1, 'Twinsen\'s House (cellar)'),
+                                makeScene(0, 'Twinsen\'s House', [
+                                    makeScene(193, 'Demo variant')
+                                ]),
+                                makeScene(1, 'Twinsen\'s House (cellar)', [
+                                    makeScene(194, 'Demo variant')
+                                ]),
                                 makeScene(9, 'Neighbour\'s House'),
                                 makeScene(14, 'Mr. Bazoo\'s shop'),
                                 makeScene(7, 'Paul\'s House'),
@@ -86,7 +90,10 @@ export const LocationsNode = {
                                 makeScene(2, 'Tralü\'s Cave'),
                                 makeScene(20, 'Tralü\'s Cave (Joe the Elf)'),
                                 makeScene(19, 'Tralü\'s Cave (small tralüs)'),
-                                makeScene(26, 'Inside the Dome')
+                                makeScene(26, 'Inside the Dome', [
+                                    makeScene(202, 'Demo variant')
+                                ]),
+                                makeScene(191, 'Esmer Spacecraft (Spying)'),
                             ],
                             props: [makeIcon('building')]
                         },
@@ -119,9 +126,15 @@ export const LocationsNode = {
                                 makeScene(38, 'Baldino\'s House'),
                                 makeScene(39, 'Bar near the Beach'),
                                 makeScene(40, 'Tickets Office'),
-                                makeScene(10, 'Temple of Bu'),
+                                makeScene(10, 'Temple of Bu', [
+                                    makeScene(207, 'Demo variant')
+                                ]),
+                                makeScene(127, 'Temple of Bu (well)'),
                                 makeScene(11, 'Temple of Bu (downstairs)'),
-                                makeScene(12, 'Temple of Bu (shuttle area)'),
+                                makeScene(12, 'Temple of Bu (spacecraft area)', [
+                                    makeScene(220, 'Demo variant')
+                                ]),
+                                makeScene(114, 'Esmer Spacecraft (Temple of Bu)'),
                                 makeScene(24, 'Hacienda'),
                                 makeScene(29, 'Hacienda (central area)'),
                                 makeScene(25, 'Hacienda (women\'s bath)'),
@@ -129,7 +142,9 @@ export const LocationsNode = {
                                 makeScene(32, 'Hacienda (secret room)'),
                                 makeScene(28, 'School of Magic (lobby)'),
                                 makeScene(27, 'School of Magic (classroom)'),
-                                makeScene(33, 'School of Magic (blowgun test)')
+                                makeScene(33, 'School of Magic (blowgun test)'),
+                                makeScene(183, 'Protection Spell cave (skeletons)'),
+                                makeScene(184, 'Protection Spell cave (monster)'),
                             ],
                             props: [makeIcon('building')]
                         },
@@ -177,7 +192,9 @@ export const LocationsNode = {
             children: [
                 {
                     name: 'Buildings',
-                    children: [],
+                    children: [
+                        makeScene(115, 'Esmer Spacecraft'),
+                    ],
                     props: [makeIcon('building')]
                 },
                 makeScene(75, 'Landing zone', [
@@ -200,7 +217,50 @@ export const LocationsNode = {
                             children: [
                                 {
                                     name: 'Buildings',
-                                    children: [],
+                                    children: [
+                                        makeScene(86, 'Spaceport (corridor)'),
+                                        makeScene(78, 'Spaceport (ticket office)'),
+                                        makeScene(41, 'Esmer Spacecraft'),
+                                        makeScene(85, 'Control Tower (stairs)'),
+                                        makeScene(83, 'Jail (lobby)'),
+                                        makeScene(84, 'Jail (cells)'),
+                                        makeScene(134, 'Bar (rehearsal room)'),
+                                        makeScene(136, 'Bar (main room)', [
+                                            makeScene(210, 'Demo variant')
+                                        ]),
+                                        makeScene(137, 'Bar (Johny Rocket)'),
+                                        makeScene(82, 'Elevators to Town Square (downstairs)'),
+                                        makeScene(139, 'Elevators to Town Square (upstairs)'),
+                                        makeScene(135, 'Casino (games room)'),
+                                        makeScene(133, 'Casino (wheel room)'),
+                                        makeScene(147, 'Shop'),
+                                        makeScene(148, 'Shop (secret room)'),
+                                        makeScene(150, 'Elevator to Upper City'),
+                                        makeScene(81, 'Hotel (lobby)'),
+                                        makeScene(79, 'Hotel (meeting room)'),
+                                        makeScene(170, 'Hotel (spy room)'),
+                                        makeScene(169, 'Hotel (woman\'s room)'),
+                                        makeScene(189, 'Emperor Spacecraft'),
+                                        makeScene(154, 'Palace (entrance)'),
+                                        makeScene(153, 'Palace (bottom-left 1, dogs)'),
+                                        makeScene(152, 'Palace (bottom-left 2, francos)'),
+                                        makeScene(151, 'Palace (left corner, dogs)'),
+                                        makeScene(155, 'Palace (top-left 1, hussar)'),
+                                        makeScene(159, 'Palace (top-left 2, time commando)'),
+                                        makeScene(163, 'Palace (top-left 3, pig farmers)', [
+                                            makeScene(217, 'Demo variant')
+                                        ]),
+                                        makeScene(156, 'Palace (middle 1)'),
+                                        makeScene(157, 'Palace (middle 2, treasure chest)'),
+                                        makeScene(160, 'Palace (middle 3, hussar)'),
+                                        makeScene(161, 'Palace (middle 4, treasure chest)'),
+                                        makeScene(158, 'Palace (bottom-right 1)'),
+                                        makeScene(162, 'Palace (bottom-right 2, hussar)'),
+                                        makeScene(164, 'Palace (top-right 1, treasure chest)'),
+                                        makeScene(165, 'Palace (top-right 2, treasure chest + chimney)'),
+                                        makeScene(166, 'Palace (right corner, francos)'),
+                                        makeScene(80, 'Palace (final boss room)'),
+                                    ],
                                     props: [makeIcon('building')]
                                 },
                                 makeScene(92, 'Spaceport', [
@@ -221,13 +281,35 @@ export const LocationsNode = {
                             ],
                             props: [makeIcon('island')]
                         },
-                        makeIslandScene(95, 'Celebration Island', []),
+                        makeIslandScene(95, 'Celebration Island', [
+                            makeScene(93, 'Temple'),
+                            makeScene(186, 'Statue (top)'),
+                            makeScene(192, 'Statue (meca-pinguins)'),
+                            makeScene(187, 'Statue (middle)', [
+                                makeScene(218, 'Demo variant')
+                            ]),
+                            makeScene(188, 'Statue (bottom, FF)'),
+                            makeScene(190, 'Statue (lava)'),
+                        ]),
                         {
                             name: 'Franco Island',
                             children: [
                                 {
                                     name: 'Buildings',
-                                    children: [],
+                                    children: [
+                                        makeScene(171, 'Shop'),
+                                        makeScene(172, 'Bar'),
+                                        makeScene(173, 'Mr. Kurtz House (retired colonel)'),
+                                        makeScene(174, 'Nursery'),
+                                        makeScene(175, 'Roger de la Fontaine\'s House'),
+                                        makeScene(140, 'Refinery (entrance)'),
+                                        makeScene(141, 'Refinery (room 2)'),
+                                        makeScene(142, 'Refinery (room 3)'),
+                                        makeScene(145, 'Refinery (tanks room)'),
+                                        makeScene(146, 'Refinery (secret room)'),
+                                        makeScene(143, 'Refinery (last room)'),
+                                        makeScene(144, 'Access to Zeppelin Port'),
+                                    ],
                                     props: [makeIcon('building')]
                                 },
                                 makeScene(109, 'Village', [
@@ -239,7 +321,15 @@ export const LocationsNode = {
                             props: [makeIcon('island')]
                         },
                         makeIslandScene(120, 'Elevator', []),
-                        makeIslandScene(110, 'Island CX', [])
+                        makeIslandScene(110, 'Island CX', [
+                            makeScene(177, 'Control Tower (stairs)'),
+                            makeScene(178, 'Control Tower (switches room)', [
+                                makeScene(221, 'Demo variant')
+                            ]),
+                            makeScene(179, 'Control Tower (entrance)'),
+                            makeScene(180, 'Corridor (dragon)'),
+                            makeScene(182, 'Emperor Spacecraft (CX)'),
+                        ])
                     ],
                     props: [makeIcon('surface')]
                 },
@@ -251,7 +341,17 @@ export const LocationsNode = {
                             children: [
                                 {
                                     name: 'Buildings',
-                                    children: [],
+                                    children: [
+                                        makeScene(123, 'Elevator Building'),
+                                        makeScene(125, 'Bank (lobby)'),
+                                        makeScene(124, 'Bank/Mine (box transit room)'),
+                                        makeScene(126, 'Bank/Mine (hatch room)'),
+                                        makeScene(111, 'Mine (gem corridor)'),
+                                        makeScene(116, 'Church (Monk assistant)'),
+                                        makeScene(119, 'Church (stairs + lava)'),
+                                        makeScene(117, 'Church (fragment room)'),
+                                        makeScene(121, 'Church (fragment room, with Monk)'),
+                                    ],
                                     props: [makeIcon('building')]
                                 },
                                 makeScene(98, 'Elvator Area', [
@@ -268,7 +368,12 @@ export const LocationsNode = {
                             children: [
                                 {
                                     name: 'Buildings',
-                                    children: [],
+                                    children: [
+                                        makeScene(106, 'Nest (lobby)'),
+                                        makeScene(128, 'Nest (blowtron test)'),
+                                        makeScene(149, 'Nest (upper exit room)'),
+                                        makeScene(176, 'Access to Island CX'),
+                                    ],
                                     props: [makeIcon('building')]
                                 },
                                 makeScene(105, 'Ferryman Area', [
@@ -284,7 +389,9 @@ export const LocationsNode = {
                             children: [
                                 {
                                     name: 'Buildings',
-                                    children: [],
+                                    children: [
+                                        makeScene(129, 'Cave'),
+                                    ],
                                     props: [makeIcon('building')]
                                 },
                                 makeScene(132, 'Ferryman Area'),
@@ -315,7 +422,15 @@ export const LocationsNode = {
                 makeScene(101, 'Broken101'),
                 makeScene(104, 'Broken104'),
                 makeScene(112, 'Broken112'),
-                makeScene(113, 'Broken113')
+                makeScene(113, 'Broken113'),
+                makeScene(118, 'Broken118'),
+                makeScene(122, 'Broken122'),
+                makeScene(167, 'Broken167'),
+                makeScene(168, 'Broken168'),
+                makeScene(181, 'Broken181'),
+                makeScene(185, 'Broken185'),
+                makeScene(214, 'Broken214'),
+                makeScene(215, 'Broken215'),
             ]
         }
     ]
