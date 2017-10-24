17f91eb960f1a1bd0454af2d071df4dc7145b44c
Adrien Grandemange <adrien.grandemange@gmail.com>
2017-10-24 02:49:32 +0200

    Make new area content beautiful

--- src/ui/editor/areas/NewArea.js
+++ src/ui/editor/areas/NewArea.js
@@ -10,8 +10,25 @@ const NewArea = {
 
 export default NewArea;
 
+const style = {
+    fontSize: 18,
+    fontWeight: 'normal',
+    cursor: 'pointer',
+    padding: 0,
+    marginLeft: 14,
+    marginTop: 14,
+    display: 'inline-block',
+    border: '1px solid white',
+    boxShadow: '0 3px 2px rgba(0, 0, 0, 0.5)'
+};
+
 function NewAreaContent(props) {
-    return <ul>{map(props.availableAreas, (area, idx) => {
-        return <li key={idx} style={{fontSize: 20, cursor: 'pointer'}} onClick={props.selectAreaContent.bind(null, area)}>{area.name}</li>;
-    })}</ul>;
+    return <div>{map(props.availableAreas, (area, idx) => {
+        return <div key={idx}
+                    style={style}
+                    onClick={props.selectAreaContent.bind(null, area)}>
+            <div style={{padding: 5, background: '#191919'}}>{area.name}</div>
+            <img style={{borderTop: '1px solid white'}} src={`editor/areas/${area.id}.png`}/>
+        </div>;
+    })}</div>;
 }
