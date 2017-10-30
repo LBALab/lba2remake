fa41bb15118158c405324b3bc7e8714a1f9f20c7
Alexandre Fontoura <xesfnet@gmail.com>
2017-10-30 22:26:13 +0000

    Adding a choice component

--- src/ui/game/AskChoice.js
+++ src/ui/game/AskChoice.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {extend} from 'lodash';
+import {extend, map} from 'lodash';
 
 import TextBox from './TextBox';
 
@@ -7,12 +7,14 @@ export default class AskChoice extends React.Component {
     constructor(props) {
         super(props);
         this.update = this.update.bind(this);
-        this.state = { content: '', offset: 0, numChoices: 0 };
+        this.state = { content: '', offset: 0, numChoices: 0, choices: [] };
         this.interval = null;
     }
 
     componentWillMount() {
-
+        this.state.choices.push('choice 1');
+        this.state.choices.push('choice 2');
+        this.state.choices.push('choice 3');
     }
 
     componentWillReceiveProps(newProps) {
@@ -28,9 +30,44 @@ export default class AskChoice extends React.Component {
     }
 
     render() {
-        return <div>
-            <TextBox text={this.props.text} />
-        </div>;
+        const color = this.props.color;
+        if (this.props.text) {
+            return <div>
+                <ul>
+                    {map(this.state.choices, (c, idx) => {
+                        return <li><Choice key={idx} text={c} color={color}/></li>
+                    })}
+                </ul>
+                <TextBox text={this.props.text}/>
+            </div>;
+        }
+        return null;
     }
 }
 
+const baseStyleChoice = {
+    position: 'absolute',
+    background: 'rgba(0, 0, 0, 0.5)',
+    fontFamily: 'LBA',
+    textShadow: 'black 4px 4px',
+    padding: 20,
+    border: '2px outset #20a2ff',
+    borderRadius: 15,
+    fontSize: '3em',
+};
+
+const styleTypeChoice = {
+    top: 30,
+    left: 100,
+    right: 100,
+    minHeight: 117
+};
+
+function Choice(props) {
+    if (props.text) {
+        const style = extend({color: props.color}, baseStyleChoice, styleTypeChoice);
+        return <div style={style}>{props.text}</div>
+    } else {
+        return null;
+    }
+}
