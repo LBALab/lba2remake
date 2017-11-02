6ae616765b973c84d53e9e63364c56f9b7cb4062
Adrien Grandemange <adrien.grandemange@gmail.com>
2017-11-02 21:02:48 +0100

    Tweaks to game variables (metadata and formatting)

--- src/ui/editor/areas/OutlinerArea/nodes/variables.js
+++ src/ui/editor/areas/OutlinerArea/nodes/variables.js
@@ -6,9 +6,13 @@ export function formatVar(varDef, value) {
     const info = getVarInfo(varDef);
     if (info) {
         if (info.type === 'boolean') {
-            return <i style={{color: value === 1 ? '#00ff00' : '#ff0000'}}>{value === 1 ? 'true' : 'false'}</i>;
+            return <span style={{color: value === 1 ? '#00ff00' : '#ff0000'}}>{value === 1 ? 'true' : 'false'}</span>;
         } else if (info.type === 'enum') {
-            return info.enumValues[value];
+            if (value in info.enumValues) {
+                return <span style={{color: '#b9a0b3'}}>{info.enumValues[value].toUpperCase()}</span>;
+            } else {
+                return <span style={{color: '#b9a0b3'}}>undefined(#{value})</span>;
+            }
         }
     }
     return value;
@@ -26,7 +30,7 @@ export const Var = {
         {
             id: 'value',
             value: varDef.value(),
-            render: (value) => formatVar(varDef, value)
+            render: (value) => <i style={{color: '#98ee92'}}>{formatVar(varDef, value)}</i>
         }
     ],
     onClick: () => {}
