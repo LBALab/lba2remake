310be019106eda33a900b783255891731b9164dc
Adrien Grandemange <adrien.grandemange@gmail.com>
2018-03-06 00:02:46 +0100

    Create basic popup system (will be used for #150)

--- src/main_game.jsx
+++ src/main_game.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Ticker from './ui/utils/Ticker';
 import Game from './ui/Game';
+import Popup from './ui/Popup';
 import {loadParams} from './params';
 
 window.onload = function() {
@@ -11,6 +12,9 @@ window.onload = function() {
     params.editor = false;
     params.noscripts = false;
     params.pauseOnLoad = false;
-    ReactDOM.render(<Game params={params} ticker={ticker} />, document.getElementById('root'));
+    ReactDOM.render(<div>
+        <Game params={params} ticker={ticker} />
+        <Popup/>
+    </div>, document.getElementById('root'));
     ticker.run();
 };
