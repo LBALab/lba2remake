eba83500463b5e2e28580ab0c3121cd897d685bf
Alexandre Fontoura <xesfnet@gmail.com>
2018-03-11 00:09:26 +0000

    #150 Adding slack webhook

--- www/metadata/game.php
+++ www/metadata/game.php
@@ -5,4 +5,24 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') die();
 $requestBody = file_get_contents('php://input');
 file_put_contents('game.json', $requestBody);
 
+
+$slackhook = 'https://hooks.slack.com/services/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';
+
+$message = json_encode(array(
+        'channel' => '#metadata',
+        'username' => 'lba2remake',
+        'text' => 'Game metadata updated.', 
+        'icon_url' => 'http://lba2remake.xesf.net/editor/icons/var.png'
+    ));
+
+$result = file_get_contents($slackhook, false, stream_context_create(array(
+    'http' => array(
+    'method' => 'POST',
+    'header' => 'Content-Type: application/json' . "\r\n",
+    'content' => $message,
+    ),
+    )));
+
+echo $result; 
+
 ?>
--- www/metadata/scene.php
+++ www/metadata/scene.php
@@ -2,7 +2,29 @@
 
 if ($_SERVER['REQUEST_METHOD'] != 'POST') die();
 
+$sceneId = $_GET['sceneId'];
+
 $requestBody = file_get_contents('php://input');
-file_put_contents("scene_" . $_GET['sceneId'] . ".json", $requestBody);
+file_put_contents("scene_" . $sceneId . ".json", $requestBody);
+
+
+$slackhook = 'https://hooks.slack.com/services/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';
+
+$message = json_encode(array(
+        'channel' => '#metadata',
+        'username' => 'lba2remake',
+        'text' => 'Scene #' . $sceneId . ' metadata updated.', 
+        'icon_url' => 'http://lba2remake.xesf.net/editor/icons/var.png'
+    ));
+
+$result = file_get_contents($slackhook, false, stream_context_create(array(
+    'http' => array(
+    'method' => 'POST',
+    'header' => 'Content-Type: application/json' . "\r\n",
+    'content' => $message,
+    ),
+    )));
+
+echo $result; 
 
 ?>
