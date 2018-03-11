f37197b34d53f5699c01489c4ea0ec8091bc5ddc
Adrien Grandemange <adrien.grandemange@gmail.com>
2018-03-11 19:46:04 +0100

    Ask for authentication information when first editing metadata (WIP: just a basic popup at this point)

--- src/ui/editor/DebugData.js
+++ src/ui/editor/DebugData.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import Popup from '../Popup';
+
 const DebugData = {
     scope: {},
     selection: {
@@ -145,12 +148,14 @@ export function loadSceneMetaData(sceneIndex, callback) {
 }
 
 function saveSceneMetaData(sceneIndex) {
-    const request = new XMLHttpRequest();
-    request.open('POST', `metadata/scene/${sceneIndex}`, true);
-    request.onload = function() {
-        console.log(`Saved scene ${sceneIndex} metadata`);
-    };
-    request.send(JSON.stringify(DebugData.metadata.scenes[sceneIndex], null, 2));
+    checkAuth((authData) => {
+        const request = new XMLHttpRequest();
+        request.open('POST', `metadata/scene/${sceneIndex}`, true);
+        request.onload = function() {
+            console.log(`Saved scene ${sceneIndex} metadata`);
+        };
+        request.send(JSON.stringify(DebugData.metadata.scenes[sceneIndex], null, 2));
+    });
 }
 
 export function loadGameMetaData() {
@@ -175,4 +180,52 @@ function saveGameMetaData() {
         console.log(`Saved game metadata`);
     };
     request.send(JSON.stringify(DebugData.metadata.game, null, 2));
+}
+
+function checkAuth(callback) {
+    let auth = null;
+    const raw_auth = localStorage.getItem('editor_auth');
+    try {
+        if (raw_auth)
+        {
+            auth = JSON.parse(raw_auth);
+        }
+    }
+    catch (e) {}
+
+    if (auth) {
+        callback(auth);
+    } else {
+        Popup.display(makeAuthPopup(callback));
+    }
+}
+
+const popup_style = {
+    background: 'rgb(45, 45, 48)',
+    border: '2px solid black',
+    color: 'white',
+    padding: 25
+};
+
+function makeAuthPopup(callback) {
+    return function AuthPopup(props) {
+        const close = () => {
+            props.close();
+            saveAuthData();
+            callback({});
+        };
+
+        return <div style={popup_style}>
+            <h1>Contributor information</h1>
+            You have just started editing the game metadata.<br/>
+            Your changes will be sent to the project authors for review.<br/>
+            Please provide your name so we can credit you for these changes.<br/>
+            Thanks for contributing!
+            <button onClick={close}>Close!</button>
+        </div>;
+    };
+}
+
+function saveAuthData(auth) {
+
 }
