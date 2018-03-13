7de52cb562a0244f2b426e997c6e8e3e38b209c9
Adrien Grandemange <adrien.grandemange@gmail.com>
2018-03-13 23:29:22 +0100

    Catch errors and display BSOD (GSOD actually)

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import Editor from './ui/Editor';
 import Popup from './ui/Popup';
 import {loadParams} from './params';
 import {loadGameMetaData} from './ui/editor/DebugData';
+import {editor as editor_style, fullscreen, center, bigButton} from './ui/styles';
+import {extend} from 'lodash';
 
 class Root extends React.Component {
     constructor(props) {
@@ -46,7 +48,41 @@ class Root extends React.Component {
 }
 
 window.onload = function() {
-    const ticker = new Ticker();
-    ReactDOM.render(<Root ticker={ticker}/>, document.getElementById('root'));
-    ticker.run();
+    init();
 };
+
+window.onerror = function(error) {
+    init(error);
+};
+
+const bsod_style = extend({}, fullscreen, editor_style.base);
+const centerVert = extend({}, center, {
+    left: 0,
+    right: 0,
+    transform: 'translate(0, -50%)',
+    textAlign: 'center'
+});
+
+function init(error) {
+    const ticker = new Ticker();
+    const reload = () => location.reload();
+    const Renderer = () => error
+        ? <div style={bsod_style}>
+            <div style={centerVert}>
+                <h1>Ooops! Something went wrong...</h1>
+                <b>Error: </b>
+                <i style={{color: 'red'}}>{error}</i>
+                <hr style={{margin: '3em 6em'}}/>
+                <button style={bigButton} onClick={diagnose.bind(null, error)}>Send diagnosis.</button>
+                <button style={bigButton} onClick={reload}>Reload app!</button>
+            </div>
+        </div>
+        : <Root ticker={ticker}/>;
+    ReactDOM.render(<Renderer/>, document.getElementById('root'));
+    ticker.run();
+}
+
+function diagnose(error) {
+    
+}
+
