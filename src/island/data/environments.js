25a088825bb4150096075d2566618420cc6839a3
Adrien Grandemange <adrien.grandemange@gmail.com>
2018-03-29 16:33:01 +0200

    #124 Fix simple indentation issues

--- src/island/data/environments.js
+++ src/island/data/environments.js
@@ -1,32 +1,32 @@
 export default {
-  TWINSUN_RAIN: {
-    index: 11,
-    skyColor: [0.0, 0.0, 0.0],
-    fogDensity: 0.6,
-    scale: 1.0
-  },
-  TWINSUN_SUNNY: {
-    index: 13,
-    skyColor: [0.51, 0.71, 0.84],
-    fogDensity: 0.2,
-    scale: 2.0
-  },
-  MOON: {
-    index: 14,
-    skyColor: [0.0, 0.07, 0.10],
-    fogDensity: 0.4,
-    scale: 128.0
-  },
-  ZEELISH: {
-    index: 16,
-    skyColor: [0.45, 0.41, 0.48],
-    fogDensity: 0.4,
-    scale: 1.0
-  },
-  UNDERGAS: {
-    index: 17,
-    skyColor: [0.44, 0.0, 0.0],
-    fogDensity: 0.4,
-    scale: 1.0
-  }
-}
+    TWINSUN_RAIN: {
+        index: 11,
+        skyColor: [0.0, 0.0, 0.0],
+        fogDensity: 0.6,
+        scale: 1.0
+    },
+    TWINSUN_SUNNY: {
+        index: 13,
+        skyColor: [0.51, 0.71, 0.84],
+        fogDensity: 0.2,
+        scale: 2.0
+    },
+    MOON: {
+        index: 14,
+        skyColor: [0.0, 0.07, 0.10],
+        fogDensity: 0.4,
+        scale: 128.0
+    },
+    ZEELISH: {
+        index: 16,
+        skyColor: [0.45, 0.41, 0.48],
+        fogDensity: 0.4,
+        scale: 1.0
+    },
+    UNDERGAS: {
+        index: 17,
+        skyColor: [0.44, 0.0, 0.0],
+        fogDensity: 0.4,
+        scale: 1.0
+    }
+};
--- src/island/data/islands.js
+++ src/island/data/islands.js
@@ -1,106 +1,106 @@
 export default [
-  {
-    name: 'CITADEL',
-    env: 'TWINSUN_RAIN',
-    startPosition: [
-      0,
-      1
-    ]
-  },
-  {
-    name: 'CITABAU',
-    env: 'TWINSUN_SUNNY',
-    startPosition: [
-      0,
-      1
-    ]
-  },
-  {
-    name: 'DESERT',
-    env: 'TWINSUN_SUNNY',
-    startPosition: [
-      0.8,
-      1.7
-    ]
-  },
-  {
-    name: 'EMERAUDE',
-    env: 'MOON',
-    startPosition: [
-      1.4,
-      0.5
-    ]
-  },
-  {
-    name: 'OTRINGAL',
-    env: 'ZEELISH',
-    startPosition: [
-      0.8,
-      -1.2
-    ]
-  },
-  {
-    name: 'KNARTAS',
-    env: 'ZEELISH',
-    startPosition: [
-      1,
-      0.8
-    ]
-  },
-  {
-    name: 'ILOTCX',
-    env: 'ZEELISH',
-    startPosition: [
-      3.3,
-      -1.2
-    ]
-  },
-  {
-    name: 'CELEBRAT',
-    env: 'ZEELISH',
-    startPosition: [
-      3.1,
-      -1.4
-    ]
-  },
-  {
-    name: 'CELEBRA2',
-    env: 'ZEELISH',
-    startPosition: [
-      3.1,
-      -1.4
-    ]
-  },
-  {
-    name: 'ASCENCE',
-    env: 'ZEELISH',
-    startPosition: [
-      2.5,
-      -1.1
-    ]
-  },
-  {
-    name: 'MOSQUIBE',
-    env: 'UNDERGAS',
-    startPosition: [
-      0.75,
-      1.24
-    ]
-  },
-  {
-    name: 'PLATFORM',
-    env: 'UNDERGAS',
-    startPosition: [
-      1.78,
-      1.47
-    ]
-  },
-  {
-    name: 'SOUSCELB',
-    env: 'UNDERGAS',
-    startPosition: [
-      1.39,
-      1.1
-    ]
-  }
-]
+    {
+        name: 'CITADEL',
+        env: 'TWINSUN_RAIN',
+        startPosition: [
+            0,
+            1
+        ]
+    },
+    {
+        name: 'CITABAU',
+        env: 'TWINSUN_SUNNY',
+        startPosition: [
+            0,
+            1
+        ]
+    },
+    {
+        name: 'DESERT',
+        env: 'TWINSUN_SUNNY',
+        startPosition: [
+            0.8,
+            1.7
+        ]
+    },
+    {
+        name: 'EMERAUDE',
+        env: 'MOON',
+        startPosition: [
+            1.4,
+            0.5
+        ]
+    },
+    {
+        name: 'OTRINGAL',
+        env: 'ZEELISH',
+        startPosition: [
+            0.8,
+            -1.2
+        ]
+    },
+    {
+        name: 'KNARTAS',
+        env: 'ZEELISH',
+        startPosition: [
+            1,
+            0.8
+        ]
+    },
+    {
+        name: 'ILOTCX',
+        env: 'ZEELISH',
+        startPosition: [
+            3.3,
+            -1.2
+        ]
+    },
+    {
+        name: 'CELEBRAT',
+        env: 'ZEELISH',
+        startPosition: [
+            3.1,
+            -1.4
+        ]
+    },
+    {
+        name: 'CELEBRA2',
+        env: 'ZEELISH',
+        startPosition: [
+            3.1,
+            -1.4
+        ]
+    },
+    {
+        name: 'ASCENCE',
+        env: 'ZEELISH',
+        startPosition: [
+            2.5,
+            -1.1
+        ]
+    },
+    {
+        name: 'MOSQUIBE',
+        env: 'UNDERGAS',
+        startPosition: [
+            0.75,
+            1.24
+        ]
+    },
+    {
+        name: 'PLATFORM',
+        env: 'UNDERGAS',
+        startPosition: [
+            1.78,
+            1.47
+        ]
+    },
+    {
+        name: 'SOUSCELB',
+        env: 'UNDERGAS',
+        startPosition: [
+            1.39,
+            1.1
+        ]
+    }
+];
