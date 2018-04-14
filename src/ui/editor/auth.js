7869227c19292e1e77a468059f4ade6474d217a5
Adrien Grandemange <adrien.grandemange@gmail.com>
2018-04-15 00:27:58 +0200

    Use id field in auth

--- src/ui/editor/auth.js
+++ src/ui/editor/auth.js
@@ -23,12 +23,20 @@ const button_style = {
     fontSize: '1.1em'
 };
 
+function validateId(auth) {
+    if (!auth.id) {
+        localStorage.removeItem('editor_auth');
+        return null;
+    }
+    return auth;
+}
+
 export function checkAuth(callback) {
     let auth = null;
     const raw_auth = localStorage.getItem('editor_auth');
     try {
         if (raw_auth) {
-            auth = JSON.parse(raw_auth);
+            auth = validateId(JSON.parse(raw_auth));
         }
     } catch (e) {
         // continue regardless of error
@@ -45,12 +53,10 @@ export function getAuthQueryString() {
     const raw_auth = localStorage.getItem('editor_auth');
     try {
         if (raw_auth) {
-            const auth = JSON.parse(raw_auth);
-            const query = [
-                `name=${encodeURIComponent(auth.name)}`,
-                `email=${encodeURIComponent(auth.email)}`
-            ];
-            return `?${query.join('&')}`;
+            const auth = validateId(JSON.parse(raw_auth));
+            if (auth) {
+                return `?userId=${auth.id}`;
+            }
         }
     } catch (e) {
         // continue regardless of error
@@ -92,8 +98,12 @@ class AuthPopup extends React.Component {
     send() {
         const auth = this.state.authData;
         if ((auth.name || auth.nocredit) && validateEmail(auth.email)) {
+            auth.id = new Date().getTime();
             if (!auth.name) {
-                auth.name = `anonymous_${new Date().getTime()}`;
+                auth.name = `anonymous_${auth.id}`;
+            }
+            if (!auth.email) {
+                auth.email = `${auth.id}@lba2remake.net`;
             }
             this.props.close();
             this.props.callback(auth);
