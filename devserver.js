2a928390c2ddbcfb1edcdecce0fd4d54ebba987e
Adrien Grandemange <adrien.grandemange@gmail.com>
2018-05-09 00:03:56 +0200

    Revert "Proxy metadata and crash_report calls to public server"
    This reverts commit 7a442c6960ce0d1d024412b79b9bfbdd3d764bae.

--- devserver.js
+++ devserver.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const {omit, clone, map} = require('lodash');
-const https = require('https');
+const fs = require('fs');
+const http = require('http');
 const React = require('react');
 const {renderToStaticMarkup} = require('react-dom/server');
 const express = require('express');
 const middleware = require('webpack-dev-middleware');
 const webpackConfig = require('./webpack.config.js');
 const webpack = require('webpack');
+const bodyParser = require('body-parser');
 require('node-jsx').install();
 const Main = require('./main.jsx');
 
@@ -16,23 +17,55 @@ const app = express();
 app.set('port', process.env.PORT || 8080);
 app.set('host', process.env.HOST || '0.0.0.0');
 
-const proxyToPublicServer = (basePath, req, res) => {
-    const queryString = map(req.query, (value, name) => `${name}=${encodeURIComponent(value)}`).join('&');
-    const tgtReq = https.request({
-        host: 'www.lba2remake.net',
-        path: `${basePath}${req.path}${queryString ? `?${queryString}` : ''}`,
-        method: req.method,
-        headers: omit(req.headers, 'host')
-    }, (tgtRes) => {
-        res.writeHead(tgtRes.statusCode, tgtRes.statusMessage, clone(tgtRes.headers));
-        tgtRes.pipe(res);
+app.use(bodyParser.json());
+
+app.post('/metadata', function (req, res) {
+    const body = req.body;
+    console.log(`Saving metadata, content=${JSON.stringify(body, null, 2)}`);
+    let fileName;
+    switch (body.type) {
+        case 'scene':
+            fileName = `./www/metadata/scene_${body.index}.json`;
+            break;
+        case 'game':
+            fileName = './www/metadata/game.json';
+            break;
+    }
+    if (fileName) {
+        fs.readFile(fileName, 'utf8', (err, file) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const content = JSON.parse(file);
+                if (!(body.subType in content)) {
+                    content[body.subType] = [];
+                }
+                content[body.subType][body.subIndex] = body.value;
+                fs.writeFile(fileName, JSON.stringify(content, null, 2), () => {});
+            }
+        });
+    }
+    res.end();
+});
+
+app.post('/crash', function (req, res) {
+    console.log('Saving crash report');
+    const report = req.body;
+    fs.writeFile('./crash_report.json', JSON.stringify(report, null, 2), () => {});
+    const content = JSON.stringify(report);
+    const tgtReq = http.request({
+        host: 'lba2remake.xesf.net',
+        path: '/ws/crash/report',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': content.length
+        }
     });
-    req.pipe(tgtReq);
-
-};
-
-app.use('/metadata', proxyToPublicServer.bind(null, '/metadata'));
-app.use('/crash', proxyToPublicServer.bind(null, '/crash'));
+    tgtReq.write(content);
+    tgtReq.end();
+    res.end();
+});
 
 app.use('/', express.static('./www'));
 
