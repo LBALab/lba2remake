4081be6d50a6e17fc1c96a5f08a7fd7e7f769cc4
Adrien Grandemange <itsmeadri@gmail.com>
2019-01-23 23:10:12 +0100

    Refactor auth popup

--- src/ui/editor/DebugData.js
+++ src/ui/editor/DebugData.js
@@ -194,25 +194,24 @@ export async function loadSceneMetaData(sceneIndex) {
     });
 }
 
-function saveMetaData(metadata) {
-    checkAuth((authData) => {
-        if (authData) {
-            const content = extend({}, metadata, {auth: authData});
-            const request = new XMLHttpRequest();
-            request.open('POST', 'metadata', true);
-            request.onload = function onload() {
-                if (this.status === 200) {
-                    // eslint-disable-next-line no-console
-                    console.log('Saved metadata:', content);
-                } else {
-                    // eslint-disable-next-line no-console
-                    console.error('Failed to save metadata');
-                }
-            };
-            request.setRequestHeader('Content-Type', 'application/json');
-            request.send(JSON.stringify(content));
-        }
-    });
+async function saveMetaData(metadata) {
+    const authData = await checkAuth();
+    if (authData) {
+        const content = extend({}, metadata, {auth: authData});
+        const request = new XMLHttpRequest();
+        request.open('POST', 'metadata', true);
+        request.onload = function onload() {
+            if (this.status === 200) {
+                // eslint-disable-next-line no-console
+                console.log('Saved metadata:', content);
+            } else {
+                // eslint-disable-next-line no-console
+                console.error('Failed to save metadata');
+            }
+        };
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(content));
+    }
 }
 
 export function loadGameMetaData() {
