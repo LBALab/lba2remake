93e007ded58f236f8fd1c62b0546dfe4a30d319d
Adrien Grandemange <itsmeadri@gmail.com>
2019-02-28 21:05:35 +0100

    Remove anims browser (will be combined with the model browser)

--- src/ui/editor/areas/model/ModelEditor.js
+++ src/ui/editor/areas/model/ModelEditor.js
@@ -1,7 +1,6 @@
 import ModelEditorContent from './ModelEditorContent';
 import {Orientation, Type} from '../../layout';
 import ModelsBrowserArea from './models/ModelsBrowserArea';
-import AnimsBrowserArea from './anims/AnimsBrowserArea';
 import InspectorArea from '../shared/InspectorArea/InspectorArea';
 
 const ModelEditor = {
@@ -14,7 +13,6 @@ const ModelEditor = {
     stateHandler: {},
     toolAreas: [
         ModelsBrowserArea,
-        AnimsBrowserArea,
         InspectorArea
     ],
     defaultLayout: {
@@ -23,15 +21,7 @@ const ModelEditor = {
         splitAt: 75,
         children: [
             { type: Type.AREA, content_id: 'model', root: true },
-            {
-                type: Type.LAYOUT,
-                orientation: Orientation.VERTICAL,
-                splitAt: 60,
-                children: [
-                    { type: Type.AREA, content_id: 'models_list' },
-                    { type: Type.AREA, content_id: 'anims_list' }
-                ]
-            }
+            { type: Type.AREA, content_id: 'models_list' },
         ]
     }
 };
--- src/ui/editor/areas/model/anims/AnimsBrowserArea.js
+++
@@ -1,8 +0,0 @@
-import {makeOutlinerArea} from '../../utils/outliner';
-
-const AnimsBrowserArea = makeOutlinerArea('anims_list', 'Animations', {
-    name: 'Animations',
-    children: []
-});
-
-export default AnimsBrowserArea;
