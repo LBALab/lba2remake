e4e15a38f7f5c48d3c302e4189b3e34360282262
Adrien Grandemange <itsmeadri@gmail.com>
2019-03-02 03:07:40 +0100

    Basic VR controls

--- src/controls/index.js
+++ src/controls/index.js
@@ -1,11 +1,16 @@
 import {makeFirstPersonMouseControls} from './mouse.ts';
 import {makeKeyboardControls} from './keyboard.ts';
 import {makeGamepadControls} from './gamepad.ts';
+import {makeVRControls} from './vr.ts';
 import {makeFirstPersonTouchControls} from './touch.ts';
 
 export function createControls(params, game, canvas, sceneManager, renderer) {
     let controls;
-    if (params.mobile && !renderer.vr) {
+    if (renderer.vr) {
+        controls = [
+            makeVRControls(game)
+        ];
+    } else if (params.mobile) {
         controls = [
             makeFirstPersonTouchControls(game),
             makeGamepadControls(sceneManager, game)
---
+++ src/controls/vr.ts
@@ -0,0 +1,31 @@
+export function makeVRControls(game: any) {
+    return {
+        type: 'vr',
+        dispose: () => {},
+        update: () => {
+            const gamepads = navigator.getGamepads();
+            let gamepad = null;
+            for (let i = 0; i < gamepads.length; i += 1) {
+                gamepad = gamepads[i];
+                // The array may contain undefined gamepads, so check for that as
+                // well as a non-null pose. VR clicker devices such as the Carboard
+                // touch handler for Daydream have a displayId but no pose.
+                if (gamepad && (gamepad.pose || gamepad.displayId)) {
+                    break;
+                } else {
+                    gamepad = null;
+                }
+            }
+            if (gamepad) {
+                const x = -gamepad.axes[0];
+                const y = -gamepad.axes[1];
+                if (Math.abs(x) < 0.8) {
+                    game.controlsState.heroSpeed = Math.sign(y);
+                } else {
+                    game.controlsState.heroSpeed = 0;
+                }
+                game.controlsState.heroRotationSpeed = x;
+            }
+        }
+    };
+}
