bb21c73079a1362a74b20f9ea435ac8e661853e1
Adrien Grandemange <itsmeadri@gmail.com>
2019-03-07 13:21:49 +0100

    Remove unimplemented island editor

--- src/ui/editor/areas/island/browser/IslandBrowserArea.js
+++
@@ -1,8 +0,0 @@
-import {makeOutlinerArea} from '../../utils/outliner';
-
-const IslandBrowserArea = makeOutlinerArea('islands_list', 'Islands', {
-    name: 'Islands',
-    children: []
-});
-
-export default IslandBrowserArea;
