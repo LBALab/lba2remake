246b49bc720ad830b21dcd54481f2d088c44869f
Adrien Grandemange <itsmeadri@gmail.com>
2019-03-20 00:22:27 +0100

    Adjust texture intensity

--- src/island/shaders/common/palette.frag.glsl
+++ src/island/shaders/common/palette.frag.glsl
@@ -1,8 +1,14 @@
-vec3 mapToPal(vec3 color, float intensity) {
-    float colorIndex = texture(lutTexture, color).a;
+vec3 getPalColor(float colorIndex, float intensity) {
     vec2 uv = vec2(
-        mod(colorIndex * 256.0, 16.0) / 16.0 + intensity / 32.0 - 0.5,
+        mod(colorIndex * 256.0, 16.0) / 16.0 + (intensity / 16.0) - 0.75,
         floor(colorIndex * 16.0) / 16.0
     );
     return texture(palette, uv + 0.03125).rgb;
+}
+
+vec3 mapToPal(vec3 color, float intensity) {
+    float colorIndex = texture(lutTexture, color).a;
+    vec3 c0 = getPalColor(colorIndex, floor(intensity));
+    vec3 c1 = getPalColor(colorIndex, ceil(intensity));
+    return mix(c0, c1, fract(intensity));
 }
