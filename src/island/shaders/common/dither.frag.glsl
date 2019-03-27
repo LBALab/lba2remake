ff36fd36cc260d248fd825ddd7d408e05c687048
Adrien Grandemange <itsmeadri@gmail.com>
2019-03-27 22:36:15 +0100

    Adjust palette UV to avoid ugly purple colors on some hardware

--- src/island/shaders/common/dither.frag.glsl
+++ src/island/shaders/common/dither.frag.glsl
@@ -1,11 +1,12 @@
 vec4 dither(float color, float intensity) {
+    const vec2 halfPixV = vec2(0.0, 0.03125);
     float nz = texture(noise, vec2(vPosition.x, vPosition.z) * 0.5).a;
     float offset = (nz - 0.5) * 2.0;
     float fintensity = floor(intensity) + offset;
     vec2 uvb = vec2(fintensity, color) * 0.0625;
     vec2 uvn = vec2(fintensity + 1.0, color) * 0.0625;
-    vec4 base = texture(palette, uvb);
-    vec4 next = texture(palette, uvn);
+    vec4 base = texture(palette, uvb + halfPixV);
+    vec4 next = texture(palette, uvn + halfPixV);
     // return vec4(vec3(nz), 1.0);
     return mix(base, next, fract(intensity));
 }
