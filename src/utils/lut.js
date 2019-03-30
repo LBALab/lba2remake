8cb10e98aefbec463de48ac79229027885b60d8b
Adrien Grandemange <itsmeadri@gmail.com>
2019-03-30 22:51:52 +0100

    Smooth LUT color transitions

--- src/utils/lut.js
+++ src/utils/lut.js
@@ -66,29 +66,34 @@ async function loadLUTData() {
 export async function generateLUTTexture({onProgress, bbs, useLabColors}) {
     const ress = await loadHqr('RESS.HQR');
     const palette = new Uint8Array(ress.getEntry(0));
+    const compPalette = useLabColors ? buildLabPalette(palette) : palette;
     const buffer = new ArrayBuffer(LUT_DIM * LUT_DIM * LUT_DIM * 16 * 4);
     const image_data = new Uint8Array(buffer);
-    const clamp = v => Math.max(Math.min(v, 15), 0);
     for (let r = 0; r < LUT_DIM; r += 1) {
         onProgress(Math.round((r / LUT_DIM) * 100));
         // eslint-disable-next-line no-await-in-loop
         await delay();
         for (let g = 0; g < LUT_DIM; g += 1) {
             for (let b = 0; b < LUT_DIM; b += 1) {
-                const tgtIdx = nearestColor([
-                    (r / LUT_DIM_M1) * 255,
-                    (g / LUT_DIM_M1) * 255,
-                    (b / LUT_DIM_M1) * 255
-                ], palette, useLabColors, bbs);
-                const x = tgtIdx % 16;
-                const y = Math.floor(tgtIdx / 16);
+                const color = getColor(useLabColors, r, g, b);
+                const distribution = decompose(color, compPalette, bbs);
                 for (let i = 0; i < 16; i += 1) {
                     const level = i * LUT_DIM * LUT_DIM * LUT_DIM * 4;
                     const idx = (r + (LUT_DIM * (g + (LUT_DIM * b)))) * 4;
-                    const pIdx = clamp(x + (i - 12)) + (y * 16);
-                    image_data[level + idx] = palette[pIdx * 3];
-                    image_data[level + idx + 1] = palette[(pIdx * 3) + 1];
-                    image_data[level + idx + 2] = palette[(pIdx * 3) + 2];
+                    const tgtColor = [0, 0, 0];
+                    for (let dIdx = 0; dIdx < 256; dIdx += 1) {
+                        const x = dIdx % 16;
+                        const y = Math.floor(dIdx / 16);
+                        const pIdx = clamp(x + (i - 12), 0, 15) + (y * 16);
+                        const weight = distribution[dIdx];
+                        tgtColor[0] += palette[pIdx * 3] * weight;
+                        tgtColor[1] += palette[(pIdx * 3) + 1] * weight;
+                        tgtColor[2] += palette[(pIdx * 3) + 2] * weight;
+                    }
+                    tgtColor.map(c => clamp(Math.round(c), 0, 255));
+                    image_data[level + idx] = tgtColor[0];
+                    image_data[level + idx + 1] = tgtColor[1];
+                    image_data[level + idx + 2] = tgtColor[2];
                 }
             }
         }
@@ -100,42 +105,83 @@ export async function generateLUTTexture({onProgress, bbs, useLabColors}) {
     return buffer;
 }
 
+function getColor(useLabColors, r, g, b) {
+    if (useLabColors) {
+        return convert.rgb.lab(
+            (r / LUT_DIM_M1) * 255,
+            (g / LUT_DIM_M1) * 255,
+            (b / LUT_DIM_M1) * 255
+        );
+    }
+    return [
+        (r / LUT_DIM_M1) * 255,
+        (g / LUT_DIM_M1) * 255,
+        (b / LUT_DIM_M1) * 255
+    ];
+}
+
 async function delay() {
     return new Promise((resolve) => {
         setTimeout(resolve, 0);
     });
 }
 
-function nearestColor([r, g, b], palette, useLabColors, bbs) {
-    const c = useLabColors ? convert.rgb.lab(r, g, b) : [r, g, b];
-    let min = Infinity;
-    let minIdx = 256;
-    for (let i = 0; i < 256; i += 1) {
-        const x = i % 16;
-        const y = Math.floor(i / 16);
-        let keep = false;
-        for (let j = 0; j < bbs.length; j += 1) {
-            const bb = bbs[j];
-            if (x >= bb.xMin && x <= bb.xMax && y >= bb.yMin && y <= bb.yMax) {
-                keep = true;
-                break;
+function clamp(v, min, max) {
+    return Math.max(Math.min(v, max), min);
+}
+
+function decompose(color, p, bbs) {
+    const distribution = [];
+    let wTotal = 0;
+    for (let y = 0; y < 16; y += 1) {
+        for (let x = 0; x < 16; x += 1) {
+            const idx = (y * 16) + x;
+            if (isAllowed(bbs, x, y)) {
+                const pColor = [
+                    p[idx * 3],
+                    p[(idx * 3) + 1],
+                    p[(idx * 3) + 2]
+                ];
+                const dist = Math.sqrt(distSq(color, pColor));
+                const weight = 1 / (1 + Math.exp(dist * 0.2));
+                distribution[idx] = weight;
+                wTotal += weight;
+            } else {
+                distribution[idx] = 0;
             }
         }
-        if (!keep) {
-            continue;
-        }
-        const pr = palette[i * 3];
-        const pg = palette[(i * 3) + 1];
-        const pb = palette[(i * 3) + 2];
-        const p = useLabColors ? convert.rgb.lab(pr, pg, pb) : [pr, pg, pb];
-        const dr = p[0] - c[0];
-        const dg = p[1] - c[1];
-        const db = p[2] - c[2];
-        const dist2 = (dr * dr) + (dg * dg) + (db * db);
-        if (dist2 < min) {
-            min = dist2;
-            minIdx = i;
+    }
+    return distribution.map(w => w / wTotal);
+}
+
+function distSq(c0, c1) {
+    const dr = c1[0] - c0[0];
+    const dg = c1[1] - c0[1];
+    const db = c1[2] - c0[2];
+    return (dr * dr) + (dg * dg) + (db * db);
+}
+
+function isAllowed(bbs, x, y) {
+    for (let j = 0; j < bbs.length; j += 1) {
+        const bb = bbs[j];
+        if (x >= bb.xMin && x <= bb.xMax && y >= bb.yMin && y <= bb.yMax) {
+            return true;
         }
     }
-    return minIdx;
+    return false;
+}
+
+function buildLabPalette(palette) {
+    const labPalette = [];
+    for (let i = 0; i < 256; i += 1) {
+        const labColor = convert.rgb.lab(
+            palette[i * 3],
+            palette[(i * 3) + 1],
+            palette[(i * 3) + 2]
+        );
+        labPalette[i * 3] = labColor[0];
+        labPalette[(i * 3) + 1] = labColor[1];
+        labPalette[(i * 3) + 2] = labColor[2];
+    }
+    return labPalette;
 }
