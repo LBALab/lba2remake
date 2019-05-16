c376bdbaad53ecfa176f693c9e02966f7b7aeaf4
Adrien Grandemange <itsmeadri@gmail.com>
2019-05-17 01:02:40 +0200

    Fix for three.js warning

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -52,14 +52,15 @@ export function createRenderer(params, canvas, rendererOptions = {}, type = 'unk
         }
     }
 
+    const rSize = new THREE.Vector2();
+
     const renderer = {
         canvas,
 
         /* @inspector(locate) */
         render: (scene) => {
-            const width = threeRenderer.getSize().width;
-            const height = threeRenderer.getSize().height;
-            scene.camera.resize(width, height);
+            threeRenderer.getSize(rSize);
+            scene.camera.resize(rSize.x, rSize.y);
             threeRenderer.render(scene.threeScene, scene.camera.threeCamera);
         },
 
