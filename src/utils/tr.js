95ef92d4e504a093d917768c612cf2c1e26b4b7b
Adrien Grandemange <itsmeadri@gmail.com>
2019-10-19 00:10:21 +0200

    Fix default translations

--- src/utils/tr.js
+++ src/utils/tr.js
@@ -2,12 +2,12 @@ import translations from './data/translations.json';
 
 export default function tr(game, id) {
     const lang = game.getState().config.language;
-    if (lang.code in translations) {
-        if (id in translations[lang.code]) {
-            return translations[lang.code][id];
-        } else if (id in translations.EN) {
-            return translations.EN[id];
-        }
+    if (lang.code in translations
+        && id in translations[lang.code]) {
+        return translations[lang.code][id];
+    }
+    if (id in translations.EN) {
+        return translations.EN[id];
     }
     return '?';
 }
