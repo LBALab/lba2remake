5d671e7863c2b592266a67a3a17c9d69ba2c34d1
Adrien Grandemange <itsmeadri@gmail.com>
2019-11-08 19:58:50 +0100

    Add debug util function

---
+++ src/utils/debug.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+
+// eslint-disable-next-line no-unused-vars
+function createDebugGizmo(object) {
+    const axesHelper = new THREE.AxesHelper(1.2);
+    axesHelper.name = 'Axes';
+    const geometry = new THREE.SphereGeometry(0.1, 32, 32);
+    const material = new THREE.MeshBasicMaterial({color: 0xffff00});
+    const sphere = new THREE.Mesh(geometry, material);
+    sphere.name = 'Sphere';
+    const gizmo = new THREE.Object3D();
+    gizmo.name = 'DebugGizmo';
+    gizmo.add(sphere);
+    gizmo.add(axesHelper);
+    return gizmo;
+}
