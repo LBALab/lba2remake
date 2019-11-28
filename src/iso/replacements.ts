6e7602c6809852717d03df83ca93639ff2cc9cbe
Adrien Grandemange <itsmeadri@gmail.com>
2019-11-28 02:17:46 +0100

    Handle bigger replacements

--- src/iso/replacements.ts
+++ src/iso/replacements.ts
@@ -23,30 +23,27 @@ export function extractGridReplacements(grid, replacements) {
                 const y = (yIdx * H) + H;
                 if (blocks[yIdx]) {
                     const layout = grid.library.layouts[blocks[yIdx].layout];
-                    if (layout && layout.index in replacements
-                        && layout.nX === 1 && layout.nZ === 1) {
+                    if (layout && layout.index in replacements) {
                         const replacement = replacements[layout.index];
-                        const threeObject = replacement.threeObject.clone();
-                        const scale = 1 / 0.75;
-                        threeObject.position.set(x + 0.5, y - H, z + 0.5);
-                        threeObject.scale.set(scale, scale, scale);
-                        const orientation = replacement.orientation;
-                        const angle = angleMapping[orientation];
-                        threeObject.quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), angle);
-                        while (yIdx < blocks.length) {
-                            if (!blocks[yIdx]) {
-                                yIdx -= 1;
-                                break;
+                        const {nX, nY, nZ} = layout;
+                        const idx = blocks[yIdx].block;
+                        const zb = Math.floor(idx / (nY * nX));
+                        const yb = Math.floor(idx / nX) - (zb * nY);
+                        const xb = idx % nX;
+                        // Check brick at the bottom corner of layout
+                        if (yb === 0 && xb === nX - 1 && zb === nZ - 1) {
+                            if (checkMatch(grid, layout, x, yIdx, z)) {
+                                suppressBricks(gridReps, layout, x, yIdx, z);
+                                addReplacementObject(
+                                    gridReps,
+                                    replacement,
+                                    x - (nX * 0.5) + 1, y - H, z - (nZ * 0.5) + 1
+                                );
+                            } else {
+                                // Log when missing match
+                                // console.log(replacement.file, xb, yb, zb);
                             }
-                            const layout2 = grid.library.layouts[blocks[yIdx].layout];
-                            if (!layout2 || layout2.index !== layout.index) {
-                                yIdx -= 1;
-                                break;
-                            }
-                            gridReps.bricks.add(`${x},${yIdx},${z}`);
-                            yIdx += 1;
                         }
-                        gridReps.objects.push(threeObject);
                     }
                 }
             }
@@ -54,4 +51,56 @@ export function extractGridReplacements(grid, replacements) {
         }
     }
     return gridReps;
+}
+
+function checkMatch(grid, layout, x, y, z) {
+    const {nX, nY, nZ} = layout;
+    for (let zL = 0; zL < nZ; zL += 1) {
+        for (let yL = 0; yL < nY; yL += 1) {
+            for (let xL = 0; xL < nX; xL += 1) {
+                const zT = z - zL;
+                const yT = y + yL;
+                const xT = x - xL;
+                const c = (zT + 1) * 64 + xT;
+                const cell = grid.cells[c];
+                const blocks = cell.blocks;
+                if (!blocks[yT]) {
+                    continue;
+                }
+                const layout2 = grid.library.layouts[blocks[yT].layout];
+                if (!layout2 || layout2.index !== layout.index) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+function suppressBricks(gridReps, layout, x, y, z) {
+    const {nX, nY, nZ} = layout;
+    for (let zL = 0; zL < nZ; zL += 1) {
+        for (let yL = 0; yL < nY; yL += 1) {
+            for (let xL = 0; xL < nX; xL += 1) {
+                const zT = z - zL;
+                const yT = y + yL;
+                const xT = x - xL;
+                gridReps.bricks.add(`${xT},${yT},${zT}`);
+            }
+        }
+    }
+}
+
+function addReplacementObject(gridReps, replacement, x, y, z) {
+    const threeObject = replacement.threeObject.clone();
+    const scale = 1 / 0.75;
+    threeObject.position.set(x, y, z);
+    threeObject.scale.set(scale, scale, scale);
+    const orientation = replacement.orientation;
+    const angle = angleMapping[orientation];
+    threeObject.quaternion.setFromAxisAngle(
+        new THREE.Vector3(0, 1, 0),
+        angle
+    );
+    gridReps.objects.push(threeObject);
 }
