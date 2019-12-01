b79fb6cad15cd2ec46f67e84453d5a16654d8092
Adrien Grandemange <itsmeadri@gmail.com>
2019-12-01 15:30:42 +0100

    Filter by libraries

--- src/ui/editor/areas/isoGrid/IsoGridEditor.ts
+++ src/ui/editor/areas/isoGrid/IsoGridEditor.ts
@@ -2,73 +2,7 @@ import IsoGridEditorContent from './IsoGridEditorContent';
 import IsoGridEditorSettings from './IsoGridEditorSettings';
 import {Orientation, Type} from '../../layout';
 import InspectorArea from '../shared/InspectorArea/InspectorArea';
-import LocationsNode from '../gameplay/locator/LocationsNode';
-import { makeOutlinerArea } from '../utils/outliner';
-import DebugData from '../../DebugData';
-import findScenePath from '../gameplay/locator/findScenePath';
-import { map, drop, each } from 'lodash';
-
-const IsoScenesNode = flattenIsoScenes(LocationsNode);
-
-function flattenIsoScenes(location) {
-    let children;
-    if (location.type === 'island') {
-        children = [];
-        collectIsoScenes(location, children);
-    } else {
-        children = map(location.children, flattenIsoScenes);
-    }
-
-    return {
-        ...location,
-        children
-    };
-}
-
-function collectIsoScenes(location, scenes, push = true) {
-    if (location.type === 'building') {
-        const newLocation = {
-            ...location,
-            children: map(location.children, c => collectIsoScenes(c, scenes, false)),
-            onClick: () => {
-                DebugData.scope.isoGridIdx = location.props[0].value;
-            },
-            selected: () => {
-                return DebugData.scope.isoGridIdx === location.props[0].value;
-            }
-        };
-        if (push) {
-            scenes.push(newLocation);
-        }
-        return newLocation;
-    }
-    each(location.children, c => collectIsoScenes(c, scenes));
-    return location;
-}
-
-const IsoBrowserArea = makeOutlinerArea('iso_browser', 'Browser', IsoScenesNode, {
-    icon: 'holomap.png',
-    frame() {
-        const isoGridIdx = DebugData.scope.isoGridIdx;
-        if (isoGridIdx !== this.isoGridIdx) {
-            this.isoGridIdx = isoGridIdx;
-            const path = findScenePath(IsoScenesNode, isoGridIdx);
-            if (path) {
-                const activePath = map(drop(path), (node: any) => node.name);
-                this.props.stateHandler.setActivePath(activePath);
-            }
-        }
-    },
-    stateHandler: {
-        setActivePath(activePath) {
-            this.setState({activePath});
-        }
-    },
-    style: {
-        background: '#111111'
-    },
-    hideRoot: true
-});
+import IsoBrowserArea from './browser/IsoBrowserArea';
 
 const IsoGridEditor = {
     id: 'iso_grid',
---
+++ src/ui/editor/areas/isoGrid/browser/IsoBrowserAreaSettings.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { map } from 'lodash';
+import LibrariesData from '../../layouts/data/libraries.json';
+
+/*
+const inputStyle = {
+    textAlign: 'center' as const,
+    verticalAlign: 'middle' as const,
+    marginRight: '1ch'
+};
+*/
+
+export default function IsoBrowserAreaSettings(props) {
+    const setLibraryFilter = e => props.stateHandler.setLibraryFilter(Number(e.target.value));
+    const selectedLib = props.sharedState.libraryFilter;
+    return <div>
+        Filter library:&nbsp;
+        <select onChange={setLibraryFilter} value={selectedLib}>
+            <option value={-1}>All</option>
+            {map(LibrariesData, lib =>
+            <option key={lib.index} value={lib.index}>{lib.name}
+            </option>)}
+        </select>
+    </div>;
+}
