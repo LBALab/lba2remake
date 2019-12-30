f6d6df995ff7fea397282490b801ee35da117bc7
Adrien Grandemange <itsmeadri@gmail.com>
2019-12-30 23:58:13 +0100

    Make sea adjust to world scale

--- src/island/shaders/env/sea.vert.glsl
+++ src/island/shaders/env/sea.vert.glsl
@@ -4,6 +4,7 @@ precision highp float;
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 uniform float time;
+uniform float worldScale;
 
 in vec3 position;
 
@@ -13,13 +14,14 @@ out vec3 vMVPos;
 
 void main() {
     vec3 pos = position;
-    float s = sin(pos.x * pos.z * 0.25 + time * 1.8) + 1.0;
-    pos.y = s * 0.144 * pos.y;
+    vec3 wPos = pos * worldScale;
+    float s = sin(wPos.x * wPos.z * 0.25 + time * 1.8) + 1.0;
+    pos.y = s * 0.144 * pos.y / worldScale;
     vec4 mPos = modelViewMatrix * vec4(pos, 1.0);
     gl_Position = projectionMatrix * mPos;
     vUv = vec2(
-        pos.x * 0.001302 + sin(time * 1.9) * 0.0001,
-        pos.z * 0.001302 + cos(time * 1.7) * 0.0001
+        wPos.x * 0.001302 + sin(time * 1.9) * 0.0001,
+        wPos.z * 0.001302 + cos(time * 1.7) * 0.0001
     );
     vMVPos = mPos.xyz;
     shore = position.y;
