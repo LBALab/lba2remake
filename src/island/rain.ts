8298879c74f841af5b79ff019caa7fc71eea64c4
Adrien Grandemange <itsmeadri@gmail.com>
2020-02-10 19:42:59 +0100

    Make rain follow Twinsen everywhere

--- src/island/rain.ts
+++ src/island/rain.ts
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import RAIN_VERT from './shaders/env/rain.vert.glsl';
 import RAIN_FRAG from './shaders/env/rain.frag.glsl';
 import { applyLightningUniforms } from './lightning';
+import SimplexNoise from 'simplex-noise';
 
 const rainMaterial = new THREE.ShaderMaterial({
     vertexShader: RAIN_VERT,
@@ -10,9 +11,11 @@ const rainMaterial = new THREE.ShaderMaterial({
     transparent: true,
     uniforms: {
         time: { value: 1.0 },
-        wind: { value: new THREE.Vector2(20, 0) },
+        wind: { value: new THREE.Vector2(0, 0) },
     }
 });
+
+const noiseGen = new SimplexNoise('LBA');
 
 export function loadRain() {
     const rainCount = 5000;
@@ -34,6 +37,7 @@ export function loadRain() {
     rainGeo.setAttribute('position', posAttr);
 
     const threeObject = new THREE.Object3D();
+    const sections = [];
     for (let x = 0; x < 3; x += 1) {
         for (let z = 0; z < 3; z += 1) {
             const section = new THREE.LineSegments(rainGeo, rainMaterial);
@@ -42,12 +46,27 @@ export function loadRain() {
             section.frustumCulled = false;
             section.position.x = x * 20;
             section.position.z = z * 20;
+            sections.push(section);
             threeObject.add(section);
         }
     }
 
-    const update = (_scene, time) => {
-        // const camPos = scene.camera.controlNode.position;
+    const update = (scene, time) => {
+        const camPos = scene.camera.controlNode.position;
+        const wind = rainMaterial.uniforms.wind.value;
+        wind.x = noiseGen.noise2D(time.elapsed * 0.05, 0) * 20 + 10;
+        wind.y = noiseGen.noise2D(time.elapsed * 0.05, 1) * 20 + 10;
+        const cX = Math.round(camPos.x / 20);
+        const cZ = Math.round(camPos.z / 20);
+        let idx = 0;
+        for (let x = -1; x < 2; x += 1) {
+            for (let z = -1; z < 2; z += 1) {
+                const section = sections[idx];
+                section.position.x = (cX + x) * 20 - wind.x;
+                section.position.z = (cZ + z) * 20 - wind.y;
+                idx += 1;
+            }
+        }
         rainMaterial.uniforms.time.value = time.elapsed;
     };
     return {threeObject, update, material: rainMaterial};
