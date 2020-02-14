8705e421419264bbd44ea8f0db5d783a3bb47681
Adrien Grandemange <itsmeadri@gmail.com>
2020-02-14 23:34:21 +0100

    Remove unused props

--- src/island/data/environments.ts
+++ src/island/data/environments.ts
@@ -2,9 +2,7 @@ export default {
     TWINSUN_RAIN: {
         index: 11,
         skyColor: [0.02, 0.02, 0.01],
-        skyOpacity: 0.5,
         fogDensity: 0.8,
-        scale: 1.0,
         rain: { count: 5000 },
         clouds: { speed: 0.04 },
         lightning: {
@@ -19,9 +17,7 @@ export default {
     TWINSUN_SUNNY: {
         index: 13,
         skyColor: [0.51, 0.71, 0.84],
-        skyOpacity: 1.0,
         fogDensity: 0.12,
-        scale: 2.0,
         clouds: {
             speed: 0.02,
             whiteness: 1,
@@ -35,9 +31,7 @@ export default {
     MOON: {
         index: 14,
         skyColor: [0.0, 0.0, 0.0],
-        skyOpacity: 1.0,
         fogDensity: 0,
-        scale: 128.0,
         stars: {
             count: 10000
         }
@@ -45,9 +39,7 @@ export default {
     ZEELISH: {
         index: 16,
         skyColor: [0.45, 0.41, 0.48],
-        skyOpacity: 0.75,
         fogDensity: 0.4,
-        scale: 1.0,
         clouds: { speed: 0.04 },
         groundClouds: {
             speed: -0.012,
@@ -59,10 +51,8 @@ export default {
     UNDERGAS: {
         index: 17,
         skyColor: [0.44, 0.0, 0.0],
-        skyOpacity: 0.2,
         skyHeight: 35,
         fogDensity: 0.45,
-        scale: 1.0,
         clouds: { speed: 0.02 },
         sea: {
             scale: 512,
