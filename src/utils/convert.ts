7cce3db30cdb1d8ba47027abcb622ca415e06c2b
Andriy Tevelyev <ndruha@gmail.com>
2020-03-05 21:33:28 +0100

    More functions to convert

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,1 +1,26 @@
-console.log("Hello world");
+import {loadHqr} from "../hqr"
+
+const videoConvertor = async () => {
+    const videos = await loadHqr("VIDEO/VIDEO.HQR");
+    console.log(videos);
+};
+
+const convertors = {
+    "video": videoConvertor,
+};
+
+const convert = () => {
+    const convertorName = process.argv[2];
+    if (!convertorName) {
+        console.error(`Please provide argument for convertor. Supported: ${Object.keys(convertors)}`);
+        return;
+    }
+    const convertor = convertors[convertorName];
+    if (!convertor) {
+        console.error(`Not supported convertor type: ${convertorName}; Supported: ${Object.keys(convertors)}`);
+        return;
+    }
+    convertor();
+};
+
+convert();
