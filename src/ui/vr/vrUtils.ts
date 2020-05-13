d6547fa4d7cb91f8dbeba3c87d0a93586c2801b7
Adrien Grandemange <itsmeadri@gmail.com>
2020-05-13 11:02:08 +0200

    Make menu's drawFrame function more generic
    This is for making labels around the VR controller model buttons

--- src/ui/vr/vrUtils.ts
+++ src/ui/vr/vrUtils.ts
@@ -1,22 +1,22 @@
-export function drawFrame(ctx, x, y, w, h, selected = true) {
+export function drawFrame(ctx, x, y, w, h, selected = true, radius = 20, lineWidth = 4) {
     ctx.clearRect(x, y, w, h);
     ctx.shadowColor = 'transparent';
     ctx.strokeStyle = 'black';
     ctx.fillStyle = selected ? 'rgb(32, 162, 255)' : 'rgba(0, 0, 0, 0.5)';
-    ctx.lineWidth = 4;
-    drawRoundRect(ctx, x + 2, y + 2, w - 4, h - 4, 20);
+    ctx.lineWidth = lineWidth;
+    drawRoundRect(ctx, x + 2, y + 2, w - 4, h - 4, radius);
     ctx.fill();
     ctx.stroke();
 }
 
-function drawRoundRect(ctx, x, y, w, h, r) {
-    if (w < 2 * r) r = w / 2;
-    if (h < 2 * r) r = h / 2;
+function drawRoundRect(ctx, x, y, w, h, radius) {
+    if (w < 2 * radius) radius = w / 2;
+    if (h < 2 * radius) radius = h / 2;
     ctx.beginPath();
-    ctx.moveTo(x + r, y);
-    ctx.arcTo(x + w, y, x + w, y + h, r);
-    ctx.arcTo(x + w, y + h, x, y + h, r);
-    ctx.arcTo(x, y + h, x, y, r);
-    ctx.arcTo(x, y, x + w, y, r);
+    ctx.moveTo(x + radius, y);
+    ctx.arcTo(x + w, y, x + w, y + h, radius);
+    ctx.arcTo(x + w, y + h, x, y + h, radius);
+    ctx.arcTo(x, y + h, x, y, radius);
+    ctx.arcTo(x, y, x + w, y, radius);
     ctx.closePath();
 }
