a8509a3e534ad20fbee0cad7cb5d8d592792ee20
Alexandre Fontoura <xesfnet@gmail.com>
2020-06-06 21:57:40 +0100

    Adding points progress bar

--- src/ui/game/BehaviourMenu.tsx
+++ src/ui/game/BehaviourMenu.tsx
@@ -7,6 +7,12 @@ interface IBehaviourMenuClover {
     leafs: number;
 }
 
+interface IBehaviourMenuMagicBall {
+    level: number;
+    strength: number;
+    bounce: number;
+}
+
 interface IBehaviourMenuProps {
     game: any;
 }
@@ -18,6 +24,7 @@ interface IBehaviourMenu {
     magic: number;
     keys: number;
     clover: IBehaviourMenuClover;
+    magicball: IBehaviourMenuMagicBall;
 }
 
 const BehaviourModeItem = (props: any) => (
@@ -28,11 +35,61 @@ const BehaviourModeItem = (props: any) => (
     </div>
 );
 
+const BehaviourMode = ({ game, behaviour }) => {
+    const behaviourText = (game.menuTexts)
+    ? game.menuTexts[80 + behaviour].value : '';
+    return (
+        <div className="behaviourMode">{behaviourText}</div>
+    );
+};
+
+const BehaviourCount = ({ type, value }: { type: 'keys' | 'money', value: number}) => {
+    return (
+        <div className={`count ${type}`}>{value}</div>
+    );
+};
+
+const BehavourPointProgress = ({ type, value, maxValue, size }
+    : { type: 'magic' | 'life', value: number, maxValue: number, size?: number }) => {
+        let maxWidth = 480;
+        if (type === 'magic') {
+            maxWidth = Math.floor(maxWidth / (4 - size));
+        }
+        const width = (value * maxWidth) / maxValue;
+    return (
+        maxValue > 0 ?
+            <div className="pointsProgress" style={{
+                    width: maxWidth,
+                }}
+            >
+                <div className={`progress ${type}`} style={{
+                    borderTopRightRadius: value === maxValue ? 15 : 0,
+                    borderBottomRightRadius: value === maxValue ? 15 : 0,
+                    width,
+                }}>
+                </div>
+            </div>
+        : null
+    );
+};
+
+const BehaviourClovers = ({ boxes, leafs}: IBehaviourMenuClover) => {
+    return (
+        <div>{boxes}{leafs}</div>
+    );
+};
+
 // @ts-ignore
 const BehaviourMenu = ({ game }: IBehaviourMenuProps) => {
-    const { behaviour, life, money, magic, keys, clover }: IBehaviourMenu = game.getState().hero;
-    const behaviourText = (game.menuTexts)
-        ? game.menuTexts[80 + behaviour].value : '';
+    const {
+        behaviour,
+        life,
+        money,
+        magic,
+        keys,
+        clover,
+        magicball
+    }: IBehaviourMenu = game.getState().hero;
     return (
         <div className="behaviourMenu">
             <div className="behaviourContainer">
@@ -44,14 +101,23 @@ const BehaviourMenu = ({ game }: IBehaviourMenuProps) => {
                         marginRight: '0px'
                     }} />
                 </div>
-                <div className="behaviourMode">{behaviourText}</div>
+                <BehaviourMode game={game} behaviour={behaviour} />
             </div>
             <div className="behaviourContainer points">
-                <div className="count money">{money}</div>
-                <div className="count keys">{keys}</div>
-                <div>{life}</div>
-                <div>{magic}</div>
-                <div>{clover.boxes}{clover.leafs}</div>
+                <BehaviourCount type="money" value={money} />
+                <BehaviourCount type="keys" value={keys} />
+                <BehavourPointProgress
+                    type="life"
+                    value={life}
+                    maxValue={255}
+                />
+                <BehavourPointProgress
+                    type="magic"
+                    value={magic}
+                    maxValue={(magicball.level) * 20}
+                    size={magicball.level}
+                />
+                <BehaviourClovers boxes={clover.boxes} leafs={clover.leafs} />
             </div>
         </div>
     );
