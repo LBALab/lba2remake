8f44bd0b27b73ad2154797b3cc000fd84f777d40
Adrien Grandemange <itsmeadri@gmail.com>
2020-08-06 00:42:00 +0200

    improve blue stars color

--- src/iso/misc/shaders/dome_stars.frag.glsl
+++ src/iso/misc/shaders/dome_stars.frag.glsl
@@ -3,7 +3,8 @@ varying float vIntensity;
 
 void main() {
     float l = 1.0 - min(length(gl_PointCoord - 0.5) * 4.0, 1.0);
-    float rg = vTint * 0.3 + 0.7;
+    vec3 blue = vec3(0.063, 0.429, 0.451);
+    vec3 color = mix(blue, vec3(1.0), vTint);
     float a = l * l * l * vIntensity;
-    gl_FragColor = vec4(vec3(rg, rg, 1.0) * a, a);
+    gl_FragColor = vec4(color * a * 8.0, a);
 }
