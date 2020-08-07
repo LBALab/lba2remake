96b56a67657e373e0f90b94306e37d70f3b322c8
Adrien Grandemange <itsmeadri@gmail.com>
2020-08-08 00:23:58 +0200

    Make stars face the camera

--- src/iso/misc/shaders/dome_stars.vert.glsl
+++ src/iso/misc/shaders/dome_stars.vert.glsl
@@ -11,10 +11,14 @@ varying vec2 vUv;
 #include <common>
 
 void main() {
-    int idx = gl_VertexID % 3;
-    vec3 pos = position;
-    pos.x += uv.x;
-    pos.z += uv.y;
+    vec3 fwd = inverseTransformDirection(vec3(0, 0, -1), modelViewMatrix);
+    vec3 cUp = inverseTransformDirection(vec3(0, 1, 0), modelViewMatrix);
+    float distToCamera = length((modelViewMatrix * vec4(position, 1.0)).xyz);
+    float m = clamp(distToCamera * 0.02, 0.0, 1.0);
+    vec3 up = mix(vec3(0, 1, 0), cUp, m);
+    vec3 right = cross(up, fwd);
+    vec2 offset = (uv - 0.5) * 1.3;
+    vec3 pos = position + up * offset.y * size + right * offset.x * size;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
     vTint = tint;
     vIntensity = intensity;
