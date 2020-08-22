1ab92def5cb792325a95299c99b2631c1be5c6f0
Adrien Grandemange <itsmeadri@gmail.com>
2020-08-22 14:05:59 +0200

    Cleanup unused texture handling code

--- src/iso/misc/shaders/dome_walls.frag.glsl
+++ src/iso/misc/shaders/dome_walls.frag.glsl
@@ -3,7 +3,6 @@ uniform vec3 color;
 uniform vec3 offset;
 uniform vec4 spot;
 
-varying vec2 vUv;
 varying vec3 vPos;
 
 void main() {
@@ -11,8 +10,6 @@ void main() {
     float s = mix(0.001, 5.0, spot.w);
     float d = 1.0 - clamp(length(vPos - pt) / s, 0.0, 1.0);
     float cf = d * 0.95 + 0.05;
-    // gl_FragColor = mix(texture(map, vUv), vec4(1.0, 0.0, 0.0, 1.0), d);
-    // gl_FragColor = mix(vec4(color, 1.0), vec4(1.0, 0.0, 0.0, 1.0), d);
     float alpha = 1.0;
     if (color.r < 0.001) {
         alpha = 1.0 - d;
