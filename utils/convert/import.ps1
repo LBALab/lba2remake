5794ea909f2fed912225ac65ad54db0ae1fdbd54
Alexandre Fontoura <xesfnet@gmail.com>
2020-10-16 21:46:25 +0100

    Missing powershel file update

--- utils/convert/import.ps1
+++ utils/convert/import.ps1
@@ -4,14 +4,14 @@ cd "$PSScriptRoot"
 npm run unpack "$path"
 if (-not $?) {throw "Failed to unpack"}
 
-npm run convert music 128 128
+npm run convert "$path" music 128 128
 if (-not $?) {throw "Failed to convert music"}
 
-npm run convert samples 128
+npm run convert "$path" samples 128
 if (-not $?) {throw "Failed to convert samples"}
 
-npm run convert voice 128
+npm run convert "$path" voice 128
 if (-not $?) {throw "Failed to convert voices"}
 
-npm run convert video
+npm run convert "$path" video
 if (-not $?) {throw "Failed to convert video"}
