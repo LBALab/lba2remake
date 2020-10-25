31543afd780c861f9cc663abf96e07a1b10fa5c3
Alexandre Fontoura <xesfnet@gmail.com>
2020-10-25 20:19:34 +0000

    Refactoring parseText to avoid code dupication

--- src/resources/parsers/text2.ts
+++ src/resources/parsers/text2.ts
@@ -1,23 +1,25 @@
 import charmaps from '../../data/charmaps.json';
 import { Resource } from '../load';
 
-export const parseTextLBA2 = (resource: Resource, index: number, language: any) => {
-    const languageIndex = index + (30 * language.index);
-    const mapData = new Uint16Array(resource.getEntry(languageIndex));
-    const data = new DataView(resource.getEntry(languageIndex + 1));
+export const parseText = (
+    resource: Resource,
+    index: number,
+    language: any,
+    skipCharacters: number
+) => {
+    const mapData = new Uint16Array(resource.getEntry(index));
+    const data = new DataView(resource.getEntry(index + 1));
+    const charmap = charmaps[language.charmap];
     const texts = {};
     let start;
     let end;
     let idx = 0;
-
-    const charmap = charmaps[language.charmap];
-
     do {
         start = data.getUint16(idx * 2, true);
         end = data.getUint16((idx * 2) + 2, true);
         const type = data.getUint8(start);
         const value = [];
-        for (let i = start + 1; i < end - 1; i += 1) {
+        for (let i = start + skipCharacters; i < end - skipCharacters; i += 1) {
             value.push(String.fromCharCode(charmap ?
                 charmap[data.getUint8(i)]
                 : data.getUint8(i))
@@ -26,6 +28,10 @@ export const parseTextLBA2 = (resource: Resource, index: number, language: any) 
         texts[mapData[idx]] = {type, index: idx, value: value.join('')};
         idx += 1;
     } while (end < data.byteLength);
-
     return texts;
+};
+
+export const parseTextLBA2 = (resource: Resource, index: number, language: any) => {
+    const languageIndex = index + (30 * language.index);
+    return parseText(resource, languageIndex, language, 1);
 };
