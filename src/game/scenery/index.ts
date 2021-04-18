b0c489e1ca4864ea3ab3e1e011f48cfc9ce15612
Alexandre Fontoura <xesfnet@gmail.com>
2021-04-18 21:04:10 +0100

    Fixed based on peer review

--- src/game/scenery/index.ts
+++ src/game/scenery/index.ts
@@ -4,8 +4,6 @@ import IsoScenery from './isometric/IsoScenery';
 import { getParams } from '../../params';
 import IsoIsland from './isometric/IsoIsland';
 
-// import { WORLD_SIZE } from '../../utils/lba';
-
 export type Scenery = Island | IsoScenery;
 
 export async function loadScenery(
@@ -14,7 +12,7 @@ export async function loadScenery(
     sceneIndex: number
 ): Promise<Scenery> {
     const isLBA1 = getParams().game === 'lba1';
-    if (sceneData.isIsland && isLBA1) { // this shouldn't be here
+    if (sceneData.isIsland && isLBA1) {
         return IsoIsland.load(game, sceneData, sceneIndex);
     }
     if (sceneData.isIsland) {
