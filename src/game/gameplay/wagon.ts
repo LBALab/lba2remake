87f246ea88a95ed0bca4ed30823c9b4f993618cc
Adrien Grandemange <itsmeadri@gmail.com>
2021-04-18 15:42:07 +0200

    Add ascii-art diagram

--- src/game/gameplay/wagon.ts
+++ src/game/gameplay/wagon.ts
@@ -4,52 +4,64 @@ import Scene from '../Scene';
 import IsoSceneryPhysics from '../scenery/isometric/IsoSceneryPhysics';
 import { Time } from '../../datatypes';
 
+/*
+**                       ----
+**        WEST       ---/    \---      NORTH
+**               ---/            \---
+**           ---/                    \---
+**        --/                            \--
+**        --\                            /--
+**           ---\                    /---
+**               ---\            /---
+**        SOUTH      ---\    /---       EAST
+**                       ----
+*/
 const RailLayout = {
     /** Temple of Bu rails **/
     // Straight
     NORTH_SOUTH:           54,
-    EAST_WEST:             53,
+    WEST_EAST:             53,
     UP_NORTH:              63,
     UP_SOUTH:              65,
-    UP_EAST:               66,
-    UP_WEST:               64,
+    UP_WEST:               66,
+    UP_EAST:               64,
     // Turns
-    TURN_NORTH_EAST:       51,
-    TURN_NORTH_WEST:       50,
-    TURN_SOUTH_EAST:       49,
-    TURN_SOUTH_WEST:       52,
+    TURN_NORTH_WEST:       51,
+    TURN_NORTH_EAST:       50,
+    TURN_SOUTH_WEST:       49,
+    TURN_SOUTH_EAST:       52,
     // Turnouts
-    TO_NORTH_NORTH_EAST:   56,
-    TO_NORTH_NORTH_WEST:   55,
-    TO_SOUTH_SOUTH_EAST:   57,
-    TO_SOUTH_SOUTH_WEST:   58,
-    TO_WEST_WEST_SOUTH:    59,
-    TO_WEST_WEST_NORTH:    60,
-    TO_EAST_EAST_SOUTH:    61,
-    TO_EAST_EAST_NORTH:    62,
+    TO_NORTH_NORTH_WEST:   56,
+    TO_NORTH_NORTH_EAST:   55,
+    TO_SOUTH_SOUTH_WEST:   57,
+    TO_SOUTH_SOUTH_EAST:   58,
+    TO_EAST_EAST_SOUTH:    59,
+    TO_EAST_EAST_NORTH:    60,
+    TO_WEST_WEST_SOUTH:    61,
+    TO_WEST_WEST_NORTH:    62,
 
     /** Undergas mine rails **/
     // Straight
     U_NORTH_SOUTH:           14,
-    U_EAST_WEST:             15,
+    U_WEST_EAST:             15,
     U_UP_NORTH:              58,
     U_UP_SOUTH:              59,
-    U_UP_EAST:               52,
-    U_UP_WEST:               57,
+    U_UP_WEST:               52,
+    U_UP_EAST:               57,
     // Turns
-    U_TURN_NORTH_EAST:       16,
-    U_TURN_NORTH_WEST:       18,
-    U_TURN_SOUTH_EAST:       19,
-    U_TURN_SOUTH_WEST:       17,
+    U_TURN_NORTH_WEST:       16,
+    U_TURN_NORTH_EAST:       18,
+    U_TURN_SOUTH_WEST:       19,
+    U_TURN_SOUTH_EAST:       17,
     // Turnouts
-    U_TO_NORTH_NORTH_EAST:   66,
-    U_TO_NORTH_NORTH_WEST:   67,
-    U_TO_SOUTH_SOUTH_EAST:   50,
-    U_TO_SOUTH_SOUTH_WEST:   60,
-    U_TO_WEST_WEST_SOUTH:    21,
-    U_TO_WEST_WEST_NORTH:    20,
-    U_TO_EAST_EAST_SOUTH:    62,
-    U_TO_EAST_EAST_NORTH:    64,
+    U_TO_NORTH_NORTH_WEST:   66,
+    U_TO_NORTH_NORTH_EAST:   67,
+    U_TO_SOUTH_SOUTH_WEST:   50,
+    U_TO_SOUTH_SOUTH_EAST:   60,
+    U_TO_EAST_EAST_SOUTH:    21,
+    U_TO_EAST_EAST_NORTH:    20,
+    U_TO_WEST_WEST_SOUTH:    62,
+    U_TO_WEST_WEST_NORTH:    64,
 };
 
 const wEuler = new THREE.Euler();
@@ -73,16 +85,10 @@ export function computeWagonMovement(scene: Scene, wagon: Actor, time: Time) {
             straight = true;
             x = 1;
             break;
-        case RailLayout.EAST_WEST:
-        case RailLayout.U_EAST_WEST:
+        case RailLayout.WEST_EAST:
+        case RailLayout.U_WEST_EAST:
             straight = true;
             z = -1;
-            break;
-        case RailLayout.TURN_SOUTH_EAST:
-        case RailLayout.U_TURN_SOUTH_EAST:
-            turn = true;
-            x = 1;
-            angle = Math.PI / 2;
             break;
         case RailLayout.TURN_SOUTH_WEST:
         case RailLayout.U_TURN_SOUTH_WEST:
@@ -90,8 +96,14 @@ export function computeWagonMovement(scene: Scene, wagon: Actor, time: Time) {
             x = 1;
             angle = Math.PI / 2;
             break;
-        case RailLayout.TURN_NORTH_EAST:
-        case RailLayout.U_TURN_NORTH_EAST:
+        case RailLayout.TURN_SOUTH_EAST:
+        case RailLayout.U_TURN_SOUTH_EAST:
+            turn = true;
+            x = 1;
+            angle = Math.PI / 2;
+            break;
+        case RailLayout.TURN_NORTH_WEST:
+        case RailLayout.U_TURN_NORTH_WEST:
             turn = true;
             z = 1;
             angle = Math.PI;
