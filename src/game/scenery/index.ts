9e38f2980558cb8d8c65f951e6bd303ade8f6e00
Alexandre Fontoura <xesfnet@gmail.com>
2021-04-18 16:28:43 +0100

    Separating IsoIsland loading

--- src/game/scenery/index.ts
+++ src/game/scenery/index.ts
@@ -1,11 +1,9 @@
-import * as THREE from 'three';
-
 import Game from '../Game';
 import Island from './island/Island';
 import IsoScenery from './isometric/IsoScenery';
 import { getParams } from '../../params';
-import { getScene } from '../../resources';
-import islandSceneMapping from './island/data/sceneMapping';
+import IsoIsland from './isometric/IsoIsland';
+
 // import { WORLD_SIZE } from '../../utils/lba';
 
 export type Scenery = Island | IsoScenery;
@@ -17,24 +15,7 @@ export async function loadScenery(
 ): Promise<Scenery> {
     const isLBA1 = getParams().game === 'lba1';
     if (sceneData.isIsland && isLBA1) { // this shouldn't be here
-        const sections = await IsoScenery.load(game, sceneData);
-        const sceneSection = islandSceneMapping[sceneIndex];
-
-        for (const index of [1, 2, 3, 4, 6]) {
-            if (index === sceneIndex) {
-                continue;
-            }
-            const data = await getScene(index);
-            const sceneMapping = islandSceneMapping[index];
-            const ssection = await IsoScenery.load(game, data);
-            const sobj = new THREE.Object3D();
-            sobj.add(...ssection.threeObject.children);
-            sobj.position.x = (sceneMapping.x - sceneSection.x) * 64;
-            sobj.position.y = (sceneMapping.y - sceneSection.y) * 64;
-            sobj.position.z = (sceneMapping.z - sceneSection.z) * 64;
-            sections.threeObject.add(sobj);
-        }
-        return sections;
+        return IsoIsland.load(game, sceneData, sceneIndex);
     }
     if (sceneData.isIsland) {
         return Island.load(game, sceneData);
