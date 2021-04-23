fba70ecef66ebb7732b1b5736b99e82eda06e429
Alexandre Fontoura <xesfnet@gmail.com>
2021-04-23 21:36:46 +0100

    Global audio filter

--- src/audio/index.ts
+++ src/audio/index.ts
@@ -21,6 +21,14 @@ function createAudioContext() {
     return new AudioContext();
 }
 
+const biquadFilter = null;
+
+function setFilter(sound) {
+    const filter = (biquadFilter) ? biquadFilter : sound.context.createBiquadFilter();
+    filter.type = 'allpass';
+    sound.setFilter(filter);
+}
+
 export function createAudioManager(state) {
     const context = createAudioContext();
     const menuContext = createAudioContext();
@@ -85,24 +93,18 @@ export function createAudioManager(state) {
             // sound.setRefDistance(20);
             // sound.setMaxDistance(10000);
             sound.setVolume(1);
-            const filter = sound.context.createBiquadFilter();
-            filter.type = 'allpass';
-            sound.setFilter(filter);
+            setFilter(sound);
             return sound;
         },
         createSamplePositionalAudioDefault: (): THREE.PositionalAudio => {
             const sound = new THREE.PositionalAudio(listener);
-            const filter = sound.context.createBiquadFilter();
-            filter.type = 'allpass';
-            sound.setFilter(filter);
+            setFilter(sound);
             return sound;
         },
         createSampleAudio: (): THREE.Audio => {
             const sound = new THREE.Audio(listener);
             sound.setVolume(1);
-            const filter = sound.context.createBiquadFilter();
-            filter.type = 'allpass';
-            sound.setFilter(filter);
+            setFilter(sound);
             return sound;
         },
 
