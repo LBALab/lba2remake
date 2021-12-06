30b6fbe98dc29ae0050dada1d587ae6938dc8c85
Adrien Grandemange <itsmeadri@gmail.com>
2021-12-06 22:11:04 +0100

    Blender baking script + export addon

---
+++ utils/blender/bake.py
@@ -0,0 +1,292 @@
+import bpy
+import platform
+import sys
+import argparse
+import os
+from mathutils import Vector
+
+#############################################################################
+###  PARSE ARGUMENTS
+#############################################################################
+
+argv = sys.argv
+argv = argv[argv.index("--") + 1:]
+
+parser = argparse.ArgumentParser(description='Perform light baking')
+parser.add_argument('--steps', default="import,bake,apply,probes,export")
+parser.add_argument('--samples', default=64, type=int)
+parser.add_argument('--resolution', default=512, type=int)
+parser.add_argument('--margin', default=2, type=int)
+parser.add_argument('--denoise', default="FAST", choices=["FAST", "ACCURATE"])
+parser.add_argument('--hdri')
+parser.add_argument('--input', required=True)
+parser.add_argument('--output')
+
+args = parser.parse_args(argv)
+
+output_file = args.output
+
+if output_file is None:
+    filename_no_ext = os.path.splitext(args.input)[0]
+    output_file = filename_no_ext + "_baked.glb"
+
+steps = args.steps
+if steps == "no_probes":
+    steps = "import,bake,apply,export"
+steps = steps.split(',')
+
+print("LIGHT BAKING")
+print(f"  STEPS: {steps}")
+print(f"  SAMPLES: {args.samples}")
+print(f"  RESOLUTION: {args.resolution}")
+print(f"  DENOISE: {args.denoise}")
+print(f"  INPUT: {args.input}")
+print(f"  OUTPUT: {output_file}")
+
+#############################################################################
+###  SETUP INITIAL SCENE
+#############################################################################
+
+bpy.data.objects.remove(bpy.data.objects["Cube"], do_unlink=True)
+bpy.data.objects.remove(bpy.data.objects["Light"], do_unlink=True)
+
+bpy.data.scenes['Scene'].use_nodes = True
+bpy.data.scenes['Scene'].render.engine = 'CYCLES'
+nodes = bpy.data.scenes['Scene'].node_tree.nodes
+nodes.new('CompositorNodeDenoise')
+nodes.new('CompositorNodeViewer')
+nodes.new('CompositorNodeOutputFile')
+nodes.new('CompositorNodeImage')
+
+links = bpy.data.scenes['Scene'].node_tree.links
+links.new(nodes["Image"].outputs[0], nodes["Denoise"].inputs[0])
+links.new(nodes["Denoise"].outputs[0], nodes["File Output"].inputs[0])
+links.new(nodes["Denoise"].outputs[0], nodes["Viewer"].inputs[0])
+
+nodes['Denoise'].prefilter = args.denoise
+nodes["Viewer"].use_alpha = False
+
+bpy.context.scene.cycles.samples = args.samples
+
+if (args.hdri is not None):
+    bpy.data.worlds['World'].use_nodes = True
+    nodes = bpy.data.worlds['World'].node_tree.nodes
+    links = bpy.data.worlds['World'].node_tree.links
+    nodes.new('ShaderNodeBackground')
+    nodes.new('ShaderNodeTexEnvironment')
+    nodes.new('ShaderNodeMapping')
+    nodes.new('ShaderNodeTexCoord')
+    nodes['Environment Texture'].image = bpy.data.images.load(args.hdri)
+
+    links.new(nodes["Background"].outputs[0], nodes["World Output"].inputs[0])
+    links.new(nodes["Environment Texture"].outputs[0], nodes["Background"].inputs[0])
+    links.new(nodes["Mapping"].outputs[0], nodes["Environment Texture"].inputs[0])
+    links.new(nodes["Texture Coordinate"].outputs[0], nodes["Mapping"].inputs[0])
+
+#############################################################################
+###  IMPORT
+#############################################################################
+print("[PROGRESS]:IMPORT:LOAD_GLB")
+filename = os.path.basename(args.input)
+bpy.ops.import_scene.gltf(filepath=args.input, files=[{"name": filename}], loglevel=50)
+
+noise_scale = 6.0
+noise_detail = 2.0
+
+# Patch ground texture material for islands
+ground_textured = bpy.data.objects.get('ground_textured')
+if ground_textured:
+    print("[PROGRESS]:IMPORT:PATCH_GROUND_TEXTURE")
+    mat = ground_textured.data.materials[0]
+    nodes = mat.node_tree.nodes
+    links = mat.node_tree.links
+
+    nodes.new(type="ShaderNodeMath")
+    nodes.new(type="ShaderNodeMixRGB").name = "MultiplyRGB"
+    nodes.new(type="ShaderNodeTexNoise")
+    nodes.new(type="ShaderNodeTexCoord")
+
+    nodes["Math"].operation = 'SUBTRACT'
+    nodes["Math"].inputs[0].default_value = 1
+    nodes['Mix'].blend_type = 'MIX'
+    nodes["MultiplyRGB"].blend_type = 'MULTIPLY'
+    nodes["MultiplyRGB"].inputs[0].default_value = 1
+    nodes['Noise Texture'].noise_dimensions = '3D'
+    nodes['Noise Texture'].inputs['Scale'].default_value = noise_scale
+    nodes['Noise Texture'].inputs['Detail'].default_value = noise_detail
+
+    links.new(nodes["Mix"].outputs[0], nodes["Principled BSDF"].inputs[0])
+    links.new(nodes["Image Texture"].outputs[1], nodes["Math"].inputs[1])
+    links.new(nodes["Image Texture"].outputs[0], nodes["Mix"].inputs[1])
+    links.new(nodes["Math"].outputs[0], nodes["Mix"].inputs[0])
+    links.new(nodes["Vertex Color"].outputs[0], nodes["MultiplyRGB"].inputs[1])
+    links.new(nodes["Texture Coordinate"].outputs[3], nodes["Noise Texture"].inputs[0])
+    links.new(nodes["Noise Texture"].outputs[0], nodes["MultiplyRGB"].inputs[2])
+    links.new(nodes["MultiplyRGB"].outputs[0], nodes["Mix"].inputs[2])
+
+ground_colored = bpy.data.objects.get('ground_colored')
+if ground_colored:
+    print("[PROGRESS]:IMPORT:PATCH_GROUND_COLORED")
+    mat = ground_colored.data.materials[0]
+    nodes = mat.node_tree.nodes
+    links = mat.node_tree.links
+
+    nodes.new(type="ShaderNodeMixRGB")
+    nodes.new(type="ShaderNodeTexNoise")
+    nodes.new(type="ShaderNodeTexCoord")
+
+    nodes["Mix"].blend_type = 'MULTIPLY'
+    nodes["Mix"].inputs[0].default_value = 1
+    nodes['Noise Texture'].noise_dimensions = '3D'
+    nodes['Noise Texture'].inputs['Scale'].default_value = noise_scale
+    nodes['Noise Texture'].inputs['Detail'].default_value = noise_detail
+
+    links.new(nodes["Vertex Color"].outputs[0], nodes["Mix"].inputs[1])
+    links.new(nodes["Mix"].outputs[0], nodes["Principled BSDF"].inputs[0])
+    links.new(nodes["Texture Coordinate"].outputs[3], nodes["Noise Texture"].inputs[0])
+    links.new(nodes["Noise Texture"].outputs[0], nodes["Mix"].inputs[2])
+
+texture_size = bpy.data.images['Image_1'].size
+textured_objects = [o for o in bpy.data.objects if o.name.startswith('objects_textured')]
+for obj in textured_objects:
+    print("[PROGRESS]:IMPORT:PATCH_OBJECT_TEXTURE")
+    mat = obj.data.materials[0]
+    nodes = mat.node_tree.nodes
+    links = mat.node_tree.links
+
+    nodes.new("ShaderNodeUVMap")
+    nodes.new("ShaderNodeUVMap")
+    nodes.new("ShaderNodeUVMap")
+    nodes.new("ShaderNodeVectorMath")
+    nodes.new("ShaderNodeVectorMath")
+    nodes.new("ShaderNodeVectorMath")
+
+    nodes['UV Map'].uv_map = 'UVMap'
+    nodes['UV Map.001'].uv_map = 'UVMap.003'
+    nodes['UV Map.002'].uv_map = 'UVMap.002'
+    nodes['Vector Math'].operation = 'MODULO'
+    nodes['Vector Math.001'].operation = 'ADD'
+    nodes['Vector Math.002'].operation = 'DIVIDE'
+    nodes['Vector Math.002'].inputs[1].default_value = Vector((texture_size[0], texture_size[1], 0))
+
+    links.new(nodes["UV Map"].outputs[0], nodes["Vector Math"].inputs[0])
+    links.new(nodes["Vector Math"].outputs[0], nodes["Vector Math.001"].inputs[0])
+    links.new(nodes["UV Map.001"].outputs[0], nodes["Vector Math"].inputs[1])
+    links.new(nodes["Vector Math.001"].outputs[0], nodes["Vector Math.002"].inputs[0])
+    links.new(nodes["UV Map.002"].outputs[0], nodes["Vector Math.001"].inputs[1])
+    links.new(nodes["Vector Math.002"].outputs[0], nodes["Image Texture"].inputs[0])
+
+
+# center view
+for screen in bpy.data.screens:
+    for area in (a for a in screen.areas if a.type == 'VIEW_3D'):
+        for region in (r for r in area.regions if r.type == 'WINDOW'):
+            override = {'screen': screen, 'area': area, 'region': region}
+            bpy.ops.view3d.view_selected(override)
+            bpy.ops.view3d.snap_cursor_to_selected(override)
+
+objects_to_bake = [o for o in bpy.data.objects if o.type == 'MESH'
+    and 'skip_baking' not in o
+    and o.data.uv_layers.get('UVMap.001')]
+
+
+#############################################################################
+###  BAKE
+#############################################################################
+if "bake" in steps:
+    print("[PROGRESS]:BAKE")
+    resolution = args.resolution
+    margin = args.margin
+    bake_target = bpy.data.images.new(name="BakeTarget", width=resolution, height=resolution, float_buffer=True)
+    for obj in objects_to_bake:
+        print(f"[PROGRESS]:BAKE:OBJECT={obj.name}")
+
+        mat = obj.data.materials[0]
+
+        bpy.ops.object.select_all(action='DESELECT')
+
+        nodes = mat.node_tree.nodes
+        links = mat.node_tree.links
+
+        node_img = nodes.new(type="ShaderNodeTexImage")
+        node_img.image = bake_target
+        uvs = nodes.new(type="ShaderNodeUVMap")
+        uvs.uv_map = 'UVMap.001'
+
+        links.new(uvs.outputs[0], node_img.inputs[0])
+
+        bpy.context.view_layer.objects.active = obj
+        obj.select_set(True)
+
+        mat.node_tree.nodes.active = node_img
+        obj.data.uv_layers['UVMap.001'].active = True
+        obj.data.use_auto_smooth = False
+
+        bpy.ops.object.bake(use_clear=False, margin=margin, type='DIFFUSE', pass_filter={'DIRECT', 'INDIRECT'})
+
+
+#############################################################################
+###  APPLY
+#############################################################################
+if "apply" in steps:
+    print("[PROGRESS]:APPLY")
+    baked_mat = bpy.data.materials.new(name="LBABakedMaterial")
+    baked_mat.use_nodes = True
+
+    nodes = baked_mat.node_tree.nodes
+    links = baked_mat.node_tree.links
+
+    scene_nodes = bpy.data.scenes["Scene"].node_tree.nodes
+    scene_nodes['Image'].image = bpy.data.images['BakeTarget']
+    tmpfilename = '/tmp/lightmap0001.exr'
+    print("Saving lightmap: ", tmpfilename)
+    scene_nodes['File Output'].base_path = '/tmp'
+    scene_nodes['File Output'].file_slots[0].path = 'lightmap'
+    scene_nodes['File Output'].format.file_format = 'OPEN_EXR'
+    scene_nodes['File Output'].format.color_mode = 'RGB'
+    scene_nodes['File Output'].format.color_depth = '32'
+    bpy.ops.render.render(animation=False, write_still=False, use_viewport=False, layer="", scene="")
+    lightmap = bpy.data.images.load(tmpfilename)
+    lightmap.name = 'Lightmap'
+
+    node_img = nodes.new(type="ShaderNodeTexImage")
+    node_img.image = bpy.data.images['Lightmap']
+
+    uvs = nodes.new(type="ShaderNodeUVMap")
+    uvs.uv_map = 'UVMap.001'
+    node_bg = nodes.new(type="ShaderNodeBackground")
+
+    links.new(uvs.outputs[0], node_img.inputs[0])
+    links.new(node_img.outputs[0], node_bg.inputs[0])
+    links.new(node_bg.outputs[0], nodes['Material Output'].inputs[0])
+
+    nodes.remove(nodes['Principled BSDF'])
+
+    for obj in objects_to_bake:
+        mesh = obj.data
+        if mesh.materials.get('LBABakedMaterial'):
+            mesh.materials['LBABakedMaterial'] = baked_mat
+        else:
+            mesh.materials.append(baked_mat)
+
+
+#############################################################################
+###  LIGHT PROBES
+#############################################################################
+if "probes" in steps:
+    print("[PROGRESS]:PROBES")
+    bpy.context.scene.render.resolution_percentage = 25
+    bpy.context.scene.render.filepath = 'light_probe.hdr'
+    bpy.context.scene.render.image_settings.file_format = 'HDR'
+    bpy.ops.render.render(use_viewport=True, write_still=True)
+
+
+#############################################################################
+###  EXPORT
+#############################################################################
+if "export" in steps:
+    print("[PROGRESS]:EXPORT")
+    bpy.ops.object.select_all(action='DESELECT')
+    for obj in objects_to_bake:
+        obj.select_set(True)
+    bpy.ops.export_scene.gltf(filepath=output_file, export_tangents=True, use_selection=True)
---
+++ utils/blender/gltf_lba_addon.py
@@ -0,0 +1,88 @@
+import typing
+import bpy
+import numpy as np
+import tempfile
+
+bl_info = {
+    "name": "LBA glTF Extension",
+    "category": "Generic",
+    "blender": (3, 0, 0),
+}
+
+def register():
+    print('Registering LBA addon')
+
+def unregister():
+    print('Unregistering LBA addon')
+
+class glTF2ExportUserExtension:
+
+    def __init__(self):
+        from io_scene_gltf2.io.com import gltf2_io
+        from io_scene_gltf2.io.com.gltf2_io_extensions import Extension
+        from io_scene_gltf2.io.exp import gltf2_io_binary_data
+        from io_scene_gltf2.blender.exp.gltf2_blender_image import TmpImageGuard
+        from io_scene_gltf2.blender.exp import gltf2_blender_search_node_tree
+        self.Extension = Extension
+        self.baked_mat_index = -1
+        self.gltf2_io = gltf2_io
+        self.gltf2_blender_search_node_tree = gltf2_blender_search_node_tree
+        self.gltf2_io_binary_data = gltf2_io_binary_data
+        self.TmpImageGuard = TmpImageGuard
+
+    def gather_mesh_hook(self, gltf2_mesh, blender_mesh, blender_object, vertex_groups, modifiers, skip_filter, material_names, export_settings):
+        baked_mat = blender_mesh.materials.get('LBABakedMaterial')
+        if baked_mat is not None:
+            if self.baked_mat_index == -1:
+                image = baked_mat.node_tree.nodes['Image Texture'].image
+                self.baked_mat_index =self.gather_image_exr(image)
+            gltf2_mat = gltf2_mesh.primitives[0].material
+            gltf2_mat.extensions['LBA2R_lightmaps'] = {}
+            gltf2_mat.extensions['LBA2R_lightmaps']['exrImageIndex'] = self.baked_mat_index
+
+    def get_tex_from_socket(self, socket):
+        result = self.gltf2_blender_search_node_tree.from_socket(
+            socket,
+            self.gltf2_blender_search_node_tree.FilterByType(bpy.types.ShaderNodeTexImage))
+        if not result:
+            return None
+        return result[0]
+
+    def gather_image_exr(self, image: bpy.types.Image):
+        buffer_view = self.gltf2_io_binary_data.BinaryData(data=self.encode(image))
+
+        image = self.gltf2_io.Image(
+            buffer_view=buffer_view,
+            extensions=None,
+            extras=None,
+            mime_type="image/x-exr",
+            name="lightmap",
+            uri="lightmap.exr"
+        )
+
+        return image
+
+    def encode(self, image: bpy.types.Image):
+        with self.TmpImageGuard() as guard:
+            self.make_temp_image_copy(guard, src_image=image)
+            tmp_image = guard.image
+            with tempfile.TemporaryDirectory() as tmpdirname:
+                tmpfilename = tmpdirname + '/img'
+                tmp_image.filepath_raw = tmpfilename
+                tmp_image.file_format = 'OPEN_EXR'
+                tmp_image.save()
+                with open(tmpfilename, "rb") as f:
+                    return f.read()
+
+    def make_temp_image_copy(self, guard, src_image: bpy.types.Image):
+        """Makes a temporary copy of src_image. Will be cleaned up with guard."""
+        guard.image = src_image.copy()
+        tmp_image = guard.image
+
+        tmp_image.update()
+
+        if src_image.is_dirty:
+            # Unsaved changes aren't copied by .copy(), so do them ourselves
+            tmp_buf = np.empty(src_image.size[0] * src_image.size[1] * 4, np.float32)
+            src_image.pixels.foreach_get(tmp_buf)
+            tmp_image.pixels.foreach_set(tmp_buf)
